cdfc53040ab93991d069f6c470e4915bbf608adf
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-06-27 18:01:06 +0300

    kotus/vanhakirjasuomi/vks2vrt.py: Converted to OO style.
    kotus/vanhakirjasuomi/vks2vrt.py:
      Converted code to object-oriented style to accommodate more easily other
        codings than the Bible: Added abstract class
        OldLiteraryFinnishToVrtConverter and concrete subclass
        BibleToVrtConverter.

--- kotus/vanhakirjasuomi/vks2vrt.py
+++ kotus/vanhakirjasuomi/vks2vrt.py
@@ -9,66 +9,90 @@ import codecs
 from optparse import OptionParser
 
 
-def process_input(f):
-    prev_fields = {'work': None, 'book': None, 'chapter': None, 'verse': None,
-                   'page': None}
-    sent_nr = 1
-    for line in f:
-        (src_code, line) = line[:-1].split(' ', 1)
-        src_fields = split_src_code(src_code)
-        sys.stdout.write(make_structure(src_fields, prev_fields,
-                                        ('first' if sent_nr == 1 else 'mid')))
-        prev_fields = src_fields
-        sys.stdout.write(make_sentence(line, sent_nr, src_code, src_fields))
-        sent_nr += 1
-    sys.stdout.write(make_structure(src_fields, prev_fields, 'last'))
+class OldLiteraryFinnishToVrtConverter(object):
+
+    _src_code_fields = []
+    _struct_levels = []
+
+    def __init__(self, opts):
+        self._opts = opts
+
+    def process_input(self, f):
+        prev_fields = dict(zip(self._src_code_fields,
+                               [None] * len(self._src_code_fields)))
+        sent_nr = 1
+        for line in f:
+            if line == '\n':
+                continue
+            (src_code, line) = line[:-1].split(' ', 1)
+            src_fields = self._split_src_code(src_code)
+            sys.stdout.write(self._make_structure(src_fields, prev_fields,
+                                                  ('first' if sent_nr == 1
+                                                   else 'mid')))
+            prev_fields = src_fields
+            sys.stdout.write(self._make_sentence(line, sent_nr, src_code,
+                                                 src_fields))
+            sent_nr += 1
+        sys.stdout.write(self._make_structure(src_fields, prev_fields, 'last'))
+
+    def _split_src_code(self, code):
+        pass
+
+    def _make_structure(self, src_fields, prev_fields, position='mid'):
+        levels = self._struct_levels
+        levelcnt = len(levels)
+        result = ''
+        close_levelnr = levelcnt
+        if position == 'last':
+            close_levelnr = 0
+        else:
+            for levelnr in xrange(0, levelcnt):
+                level = levels[levelnr]
+                if (src_fields[level] != prev_fields[level]
+                    and prev_fields[level] is not None):
+                    close_levelnr = levelnr
+                    break
+        # result += 'CLOSE: ' + str(close_levelnr) + '\n'
+        for levelnr in xrange(levelcnt - 1, close_levelnr - 1, -1):
+            result += '</' + levels[levelnr] + '>\n'
+        open_levelnr = 0 if position == 'first' else close_levelnr
+        if position != 'last':
+            for levelnr in xrange(open_levelnr, levelcnt):
+                level = levels[levelnr]
+                result += '<' + level + ' code="' + src_fields[level] + '">\n'
+        return result
+
+    def _make_sentence(self, line, sent_nr, src_code, src_fields):
+        return (u'<sentence id="{0}" code="{1}" page="{2}">\n'
+                .format(sent_nr, src_code, src_fields['page'])
+                + line.replace(' ', '\n')
+                + '</sentence>\n')
 
 
+class BibleToVrtConverter(OldLiteraryFinnishToVrtConverter):
 
-def split_src_code(code):
-    (work, book, position, page) = code.split('-')
-    (chapter, verse) = (position.split(':') if ':' in position
-                        else (position, ''))
-    return {'work': work,
-            'book': book,
-            'chapter': chapter,
-            'verse': verse,
-            'page': page}
+    _struct_levels = ['work', 'book', 'chapter', 'verse']
+    _src_code_fields = ['work', 'book', 'chapter', 'verse', 'page']
 
+    def __init__(self, opts):
+        OldLiteraryFinnishToVrtConverter.__init__(self, opts)
 
-def make_structure(src_fields, prev_fields, position='mid'):
-    levels = ['work', 'book', 'chapter', 'verse']
-    result = ''
-    close_levelnr = len(levels)
-    if position == 'last':
-        close_levelnr = 0
-    else:
-        for levelnr in xrange(0, len(levels)):
-            level = levels[levelnr]
-            if (src_fields[level] != prev_fields[level]
-                and prev_fields[level] is not None):
-                close_levelnr = levelnr
-                break
-    # result += 'CLOSE: ' + str(close_levelnr) + '\n'
-    for levelnr in xrange(len(levels) - 1, close_levelnr - 1, -1):
-        result += '</' + levels[levelnr] + '>\n'
-    open_levelnr = 0 if position == 'first' else close_levelnr
-    if position != 'last':
-        for levelnr in xrange(open_levelnr, len(levels)):
-            level = levels[levelnr]
-            result += '<' + level + ' code="' + src_fields[level] + '">\n'
-    return result
-
-
-def make_sentence(line, sent_nr, src_code, src_fields):
-    return (u'<sentence id="{0}" code="{1}" page="{2}">\n'
-            .format(sent_nr, src_code, src_fields['page'])
-            + line.replace(' ', '\n')
-            + '</sentence>\n')
+    def _split_src_code(self, code):
+        (work, book, position, page) = code.split('-')
+        (chapter, verse) = (position.split(':') if ':' in position
+                            else (position, ''))
+        return {'work': work,
+                'book': book,
+                'chapter': chapter,
+                'verse': verse,
+                'page': page}
 
 
 def getopts():
     optparser = OptionParser()
+    optparser.add_option('--mode', '-m', type='choice',
+                         choices=['biblia', 'laws', 'sermons'],
+                         default='biblia')
     (opts, args) = optparser.parse_args()
     return (opts, args)
 
@@ -79,8 +103,10 @@ def main():
     sys.stdin = codecs.getreader(input_encoding)(sys.stdin)
     sys.stdout = codecs.getwriter(output_encoding)(sys.stdout)
     (opts, args) = getopts()
-    process_input(sys.stdin)
-    
+    if opts.mode == 'biblia':
+        converter = BibleToVrtConverter(opts)
+    converter.process_input(sys.stdin)
+
 
 if __name__ == "__main__":
     main()
