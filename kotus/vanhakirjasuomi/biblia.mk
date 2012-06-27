44b4f68e290f352c944665a804a2780cf7fef1d1
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-06-27 16:50:41 +0300

    kotus/vanhakirjasuomi: Call biblia.mk from Makefile.
    kotus/vanhakirjasuomi/Makefile:
      Call all .mk files in the directory and set CORPNAME based on the makefile
        name.
      Removed Biblia-specific parts.
    
    kotus/vanhakirjasuomi/biblia.mk:
      Removed setting CORPNAME (comes from Makefile).
      Set FILES to the list of source files.

--- kotus/vanhakirjasuomi/biblia.mk
+++ kotus/vanhakirjasuomi/biblia.mk
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 
 
-CORPNAME = vks
-
-
 include ../../corp-common.mk
 
+
+FILES = vt4_prof.sen
 
 P_ATTRS = 
 S_ATTRS = work:0+code book:0+code chapter:0+code verse:0+code \
 		sentence:0+id+code+page
 
 
-$(CORPNAME).vrt: vt4_prof.sen
-	iconv -flatin1 $< \
+$(CORPNAME).vrt: $(FILES)
+	iconv -flatin1 $^ \
 	| ./vks2vrt.py > $@
