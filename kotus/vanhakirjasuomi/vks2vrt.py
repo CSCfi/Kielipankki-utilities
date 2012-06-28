4e3221dfa0ee0825785d571c867b45103883878c
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-06-28 10:50:09 +0300

    kotus/vanhakirjasuomi/vks2vrt.py: Support laws and sermons subcorpora.
    kotus/vanhakirjasuomi/vks2vrt.py:
      OldLiteraryFinnishToVrtConverter:
        Moved _src_code_fields and _struct_levels to instance attributes.
        process_input(): Strip leading and trailing whitespace from line.
        _make_sentence(): Call _make_word_attrs and _split_words.
        Added _split_words(): Split line into words.
        Added _make_word_attrs(): Return the attributes for a word.
      BibleToVrtConverter:
        Initialize _struct_levels and _src_code_fields in __init__().
      Added class LawsAndSermonsToVrtConverter for the laws and sermons subcorpora:
        Use a simpler _split_src_code() and _make_word_attrs() handling addition
          markers.
      main(): Handle --mode=laws and --mode=sermons.

--- kotus/vanhakirjasuomi/vks2vrt.py
+++ kotus/vanhakirjasuomi/vks2vrt.py
@@ -4,6 +4,7 @@
 
 import sys
 import codecs
+import re
 
 
 from optparse import OptionParser
@@ -11,20 +12,20 @@ from optparse import OptionParser
 
 class OldLiteraryFinnishToVrtConverter(object):
 
-    _src_code_fields = []
-    _struct_levels = []
-
     def __init__(self, opts):
         self._opts = opts
+        self._src_code_fields = []
+        self._struct_levels = []
 
     def process_input(self, f):
         prev_fields = dict(zip(self._src_code_fields,
                                [None] * len(self._src_code_fields)))
         sent_nr = 1
         for line in f:
-            if line == '\n':
+            line = line.strip()
+            if line == '':
                 continue
-            (src_code, line) = line[:-1].split(' ', 1)
+            (src_code, line) = line.split(' ', 1)
             src_fields = self._split_src_code(src_code)
             sys.stdout.write(self._make_structure(src_fields, prev_fields,
                                                   ('first' if sent_nr == 1
@@ -65,17 +66,23 @@ class OldLiteraryFinnishToVrtConverter(object):
     def _make_sentence(self, line, sent_nr, src_code, src_fields):
         return (u'<sentence id="{0}" code="{1}" page="{2}">\n'
                 .format(sent_nr, src_code, src_fields['page'])
-                + line.replace(' ', '\n')
+                + '\n'.join([self._make_word_attrs(word)
+                             for word in self._split_words(line)]) + '\n'
                 + '</sentence>\n')
+
+    def _split_words(self, line):
+        return line.split(' ')
+
+    def _make_word_attrs(self, word):
+        return word
 
 
 class BibleToVrtConverter(OldLiteraryFinnishToVrtConverter):
 
-    _struct_levels = ['work', 'book', 'chapter', 'verse']
-    _src_code_fields = ['work', 'book', 'chapter', 'verse', 'page']
-
     def __init__(self, opts):
         OldLiteraryFinnishToVrtConverter.__init__(self, opts)
+        self._struct_levels = ['work', 'book', 'chapter', 'verse']
+        self._src_code_fields = self._struct_levels + ['page']
 
     def _split_src_code(self, code):
         (work, book, position, page) = code.split('-')
@@ -86,6 +93,26 @@ class BibleToVrtConverter(OldLiteraryFinnishToVrtConverter):
                 'chapter': chapter,
                 'verse': verse,
                 'page': page}
+
+
+class LawsAndSermonsToVrtConverter(OldLiteraryFinnishToVrtConverter):
+
+    def __init__(self, opts, struct_top='source'):
+        OldLiteraryFinnishToVrtConverter.__init__(self, opts)
+        self._struct_top = struct_top
+        self._struct_levels = [struct_top]
+        self._src_code_fields = self._struct_levels + ['page']
+
+    def _split_src_code(self, code):
+        (law, page) = code.split('-', 1)
+        return {self._struct_top: law,
+                'page': page}
+
+    def _make_word_attrs(self, word):
+        word = word.replace('<', '[').replace('>', ']')
+        compl_word = word.replace('~', '')
+        orig_word = re.sub(r'.~', '', word)
+        return '\t'.join([word, orig_word, compl_word])
 
 
 def getopts():
@@ -105,6 +132,10 @@ def main():
     (opts, args) = getopts()
     if opts.mode == 'biblia':
         converter = BibleToVrtConverter(opts)
+    elif opts.mode == 'laws':
+        converter = LawsAndSermonsToVrtConverter(opts, 'law')
+    elif opts.mode == 'sermons':
+        converter = LawsAndSermonsToVrtConverter(opts, 'source')
     converter.process_input(sys.stdin)
 
 
