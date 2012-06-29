d77f68cf31f98733da8453a45804de7707fc679f
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-06-29 12:21:01 +0300

    kotus/varhaisnykysuomi/asetus.mk: Changed structural attributes.
    kotus/varhaisnykysuomi/asetus.mk:
      Changed structural attributes to reflect the changes in VRT file coding.

--- kotus/varhaisnykysuomi/asetus.mk
+++ kotus/varhaisnykysuomi/asetus.mk
@@ -6,6 +6,7 @@ MODE = statute
 FILES = 1840heik.xml
 
 P_ATTRS = 
-S_ATTRS = text:0+title+distributor div:3+id+type sentence:0+id+type
+S_ATTRS = text:0+distributor+source+title article:0+id paragraph:0+id \
+		sentence:0+id+type hi:0+rend dateline:0
 
 include vns-common.mk
