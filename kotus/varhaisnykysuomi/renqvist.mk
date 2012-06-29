0e780bbf3854fcaff8ab5a76dbb1526c57e5ad8d
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-06-29 16:20:50 +0300

    kotus/varhaisnykysuomi: Added renqvist.mk for Renqvist subcorpus.
    Added kotus/varhaisnykysuomi/renqvist.mk:
      Makefile for the Renqvist subcorpus.

---
+++ kotus/varhaisnykysuomi/renqvist.mk
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+VNS2VRT_OPTS = 
+
+FILES = viinan1835.xml
+
+P_ATTRS = 
+S_ATTRS = text:0+distributor+source+title para:0+id+type sentence:0+id+n pb:0
+
+include vns-common.mk
