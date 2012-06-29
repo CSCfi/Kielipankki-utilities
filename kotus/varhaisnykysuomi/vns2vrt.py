395672f2a6f45e2471b716d8a08f9ae2b3917cbc
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-06-29 16:15:47 +0300

    kotus/varhaisnykysuomi/vns2vrt.py: Support sentence-tagged text.
    kotus/varhaisnykysuomi/vns2vrt.py:
      Added support for sentence-tagged text.
      Converter:
        __init__(): Added parameter divtypemap.
        process_body_elem(): Handle sentences tagged as s. Use self._divtypemap
          instead of hard-coded dictionary. Call _process_subelems(). By default
          copy elements as such.
        Added _make_sentence(): Make a sentence from an element.
        Added _process_subelems(): Process the subelements of an element.
        _tokenize(): Tokenize only if self._opts.tokenize.
        _add_content_newlines(): Moved repreated code to _add_lead_trail_newline().
      getopts(): Changed default mode to "sentences". Added options --tokenize and
        --paragraph-as-sentence and set them to True if mode is "statute".
      main(): Define divtypemaps for modes and pass them to Converter.

--- kotus/varhaisnykysuomi/vns2vrt.py
+++ kotus/varhaisnykysuomi/vns2vrt.py
@@ -13,8 +13,9 @@ from optparse import OptionParser
 
 class Converter(object):
 
-    def __init__(self, opts):
+    def __init__(self, opts, divtypemap={}):
         self._opts = opts
+        self._divtypemap = divtypemap
 
     def process_input(self, f):
         self._src_etr = et.parse(f)
@@ -53,30 +54,48 @@ class Converter(object):
         result = None
         id_attr = body_e.get('id', '')
         # print '<' + body_e.tag + " id=" + id_attr
-        if body_e.tag in ['p', 'head', 'opener']:
-            result = et.Element('sentence')
-            self._process_mixed_content(body_e, result)
+        if body_e.tag == 's':
+            result = self._make_sentence(body_e)
+        elif body_e.tag in ['p', 'head', 'opener']:
+            if self._opts.para_as_sent:
+                result = self._make_sentence(body_e)
+            else:
+                result = et.Element('para')
+                subresults = self._process_subelems(body_e)
+                result.extend(subresults)
             result.set('type', body_e.tag)
         elif body_e.tag == 'div':
             type_ = body_e.get('type', '')
-            subresults = [subresult
-                          for subelem in body_e
-                          for subresult in self._process_body_elem(subelem)]
+            subresults = self._process_subelems(body_e)
             if not type_:
                 return subresults
             else:
-                tag = {'artikla': 'article', u'pykälä': 'paragraph'}[type_]
+                tag = self._divtypemap.get(type_, "div")
                 result = et.Element(tag)
                 # result.set('type', type_)
                 result.extend(subresults)
                 # print len(result)
         elif body_e.tag == 'body':
             return self._process_body_elem(body_e[0])
+        else:
+            result = et.Element(body_e.tag)
+            subresults = self._process_subelems(body_e)
+            result.extend(subresults)
         result.set('id', id_attr)
         self._add_content_newlines(result)
         # print "<<" + result.text + ">>"
         # print '>' + body_e.tag + " id=" + id_attr + " " + result.get('id')
         return [result]
+
+    def _make_sentence(self, elem):
+        result = et.Element('sentence')
+        self._process_mixed_content(elem, result)
+        return result
+
+    def _process_subelems(self, elem):
+        return [subresult
+                for subelem in elem
+                for subresult in self._process_body_elem(subelem)]
 
     def _process_text(self, elem, result):
         result.text = self._tokenize(self._get_all_text(elem))
@@ -96,28 +115,42 @@ class Converter(object):
                                     for subelem in elem])
 
     def _tokenize(self, text):
-        text = re.sub(r'([.,:;])[ \n]', r' \1 ', text)
+        text = text or ''
+        if self._opts.tokenize:
+            text = re.sub(r'([.,:;])[ \n]', r' \1 ', text)
         return '\n'.join(text.split()) + '\n'
 
     def _add_content_newlines(self, elem):
-        elem.text = elem.text or ''
-        if not elem.text.startswith('\n'):
-            elem.text = '\n' + elem.text
-        elem.tail = elem.tail or ''
-        if not elem.tail.endswith('\n'):
-            elem.tail += '\n'
+        elem.text = self._add_lead_trail_newline(elem.text)
+        elem.tail = self._add_lead_trail_newline(elem.tail)
+
+    def _add_lead_trail_newline(self, s):
+        s = s or '\n'
+        if s[0] != '\n':
+            s = '\n' + s
+        if s[-1] != '\n':
+            s += '\n'
+        return s
 
 
 def getopts():
     optparser = OptionParser()
     optparser.add_option('--mode', '-m', type='choice',
-                         choices=['statute'],
-                         default='statute')
+                         choices=['sentences', 'statute'],
+                         default='sentences')
+    optparser.add_option('--tokenize', action='store_true', default=False)
+    optparser.add_option('--para-as-sent', '--paragraph-as-sentence',
+                         action='store_true', default=False)
     (opts, args) = optparser.parse_args()
+    if opts.mode == 'statute':
+        opts.tokenize = True
+        opts.para_as_sent = True
     return (opts, args)
 
 
 def main():
+    divtypemaps = {'statute': {'artikla': 'article', u'pykälä': 'paragraph'},
+                   'sentences': {}}
     input_encoding = 'utf-8'
     output_encoding = 'utf-8'
     # ElementTree.XMLParser uses the encoding specified in the XML
@@ -125,7 +158,7 @@ def main():
     # sys.stdin = codecs.getreader(input_encoding)(sys.stdin)
     # sys.stdout = codecs.getwriter(output_encoding)(sys.stdout)
     (opts, args) = getopts()
-    converter = Converter(opts)
+    converter = Converter(opts, divtypemap=divtypemaps.get(opts.mode, {}))
     converter.process_input(args[0] if args else sys.stdin)
 
 
