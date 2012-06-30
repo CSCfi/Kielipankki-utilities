912a46bc773ec8e52262612e19e389e66d0d955c
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-06-30 11:00:19 +0300

    kotus/varhaisnykysuomi: Added renvall.mk for the Renvall dictionary.
    kotus/varhaisnykysuomi/renvall.mk:
      A makefile for the Renvall dictionary.

---
+++ kotus/varhaisnykysuomi/renvall.mk
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+VNS2VRT_OPTS = --mode=dictionary --tokenize --dict-info-as-sent-attrs
+
+FILES = renvall.xml
+
+P_ATTRS = 
+S_ATTRS = text:0+distributor+source+title \
+		sentence:0+etym+etymlang+example+form+pos+xref \
+		item:0+itemtype+type+lang
+
+include vns-common.mk
