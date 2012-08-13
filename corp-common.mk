8ef93172260e9819735e875b4e9233a5ca1fdd07
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-08-13 13:13:33 +0300

    corp-common.mk: Avoid unnecessary remaking. Use cwb-make. Misc edits.
    corp-common.mk:
      Defined CORPGROUP for the group of CWB files.
      Use cwb-make instead of cwb-makeall.
      MAKE_CORPUS_R: Pass value of DB to sub-makes.
      Use the .info file as a timestamp file.
      Use timestamp files for database updates to avoid unnecessary updating.

--- corp-common.mk
+++ corp-common.mk
@@ -9,13 +9,17 @@ CORPNAME_U := $(shell echo $(CORPNAME) | perl -pe 's/(.*)/\U$$1\E/')
 
 DBUSER = korp
 DBNAME = korp
+# Unix group for CWB corpus files
+CORPGROUP = korp
 
 CWBDIR = /usr/local/cwb/bin
 CORPDIR = /corpora/data
+CORPCORPDIR = $(CORPDIR)/$(CORPNAME)
 REGDIR = /corpora/registry
 
-CWB_ENCODE = $(CWBDIR)/cwb-encode -d $(CORPDIR)/$(CORPNAME) -f $(CORPNAME).vrt -R $(REGDIR)/$(CORPNAME) -xsB -c utf8
+CWB_ENCODE = $(CWBDIR)/cwb-encode -d $(CORPCORPDIR) -f $(CORPNAME).vrt -R $(REGDIR)/$(CORPNAME) -xsB -c utf8
 CWB_MAKEALL = $(CWBDIR)/cwb-makeall -V -r $(REGDIR) $(CORPNAME_U)
+CWB_MAKE = cwb-make -r $(REGDIR) -g $(CORPGROUP) -M 2000 $(CORPNAME_U)
 
 P_OPTS = $(foreach attr,$(P_ATTRS),-P $(attr))
 S_OPTS = $(foreach attr,$(S_ATTRS),-S $(attr))
@@ -33,7 +37,7 @@ all: $(TARGETS)
 
 define MAKE_CORPUS_R
 $(1)$(if $(subst @,,$(2)),@$(2)):
-	$$(MAKE) -f $(1).mk $(subst @,all,$(2)) CORPNAME=$(1)
+	$$(MAKE) -f $(1).mk $(subst @,all,$(2)) CORPNAME=$(1) DB=$(DB)
 endef
 
 $(foreach corp,$(CORPORA),\
@@ -41,14 +45,19 @@ $(foreach corp,$(CORPORA),\
 		$(eval $(call MAKE_CORPUS_R,$(corp),$(targ)))))
 
 
-reg: vrt $(CORPNAME).info
-	$(CWB_MAKEALL)
-	cp -p $(CORPNAME).info $(CORPDIR)/$(CORPNAME)/.info
+reg: vrt
+	$(CWB_MAKE)
 
-vrt: $(CORPNAME).vrt
-	-mkdir $(CORPDIR)/$(CORPNAME)
-	-rm $(CORPDIR)/$(CORPNAME)/*
-	$(CWB_ENCODE) $(P_OPTS) $(S_OPTS)
+vrt: $(CORPCORPDIR)/.info
+
+# The info file $(CORPCORPDIR)/.info is (ab)used as a timestamp file
+# to avoid unnecessarily remaking the corpus data if the .vrt file has
+# not changed.
+
+$(CORPCORPDIR)/.info: $(CORPNAME).vrt $(CORPNAME).info
+	-mkdir $(CORPCORPDIR) || rm $(CORPCORPDIR)/*
+	$(CWB_ENCODE) $(P_OPTS) $(S_OPTS) \
+	&& cp $(CORPNAME).info $(CORPCORPDIR)/.info
 
 %.info: %.vrt
 	echo "Sentences: "`egrep -c '^<sentence[> ]' $<` > $@
@@ -59,21 +68,27 @@ db: korp_db
 
 korp_db: korp_rels korp_lemgrams
 
+korp_rels: $(CORPNAME)_rels_load.timestamp
+
 # MySQL statement "load data local infile" gives an error message on
 # Daniel: "ERROR 1148 (42000) at line 1: The used command is not
 # allowed with this MySQL version". mysqlimport does the same thing.
 
-korp_rels: $(CORPNAME)_rels.tsv
+$(CORPNAME)_rels_load.timestamp: $(CORPNAME)_rels.tsv
 	mysql --user $(DBUSER) --execute 'CREATE TABLE IF NOT EXISTS `relations_$(CORPNAME_U)` (`head` varchar(1024) NOT NULL, `rel` char(3) NOT NULL, `dep` varchar(1024) NOT NULL, `depextra` varchar(1024) DEFAULT NULL, `freq` int(11) NOT NULL, `freq_rel` int(11) NOT NULL, `freq_head_rel` int(11) NOT NULL, `freq_rel_dep` int(11) NOT NULL, `wf` tinyint(4) NOT NULL, `sentences` text, KEY `head` (`head`(255)), KEY `dep` (`dep`(255))) ENGINE=InnoDB DEFAULT CHARSET=utf8;' $(DBNAME)
 	mysql --user $(DBUSER) --execute "truncate table relations_$(CORPNAME_U);" $(DBNAME)
 	-ln -sf $(CORPNAME)_rels.tsv relations_$(CORPNAME_U).tsv
-	mysqlimport --user $(DBUSER) --local korp relations_$(CORPNAME_U)
+	mysqlimport --user $(DBUSER) --local korp relations_$(CORPNAME_U).tsv
+	touch $@
 
-korp_lemgrams: $(CORPNAME)_lemgrams.tsv
-	ln -s $< lemgram_index.tsv
+korp_lemgrams: $(CORPNAME)_lemgrams_load.timestamp
+
+$(CORPNAME)_lemgrams_load.timestamp: $(CORPNAME)_lemgrams.tsv
+	ln -sf $< lemgram_index.tsv
 	mysql --user $(DBUSER) --execute "delete from lemgram_index where corpus='$(CORPNAME_U)';" $(DBNAME)
 	mysqlimport --user $(DBUSER) --local korp lemgram_index.tsv
 	rm lemgram_index.tsv
+	touch $@
 
 $(CORPNAME)_lemgrams.tsv: $(CORPNAME).vrt
 	egrep -v '<' $< \
