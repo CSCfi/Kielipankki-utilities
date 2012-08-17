20f9e3f65623b07084c318206160bd78a4af51fd
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-08-17 16:28:44 +0300

    kotus/nykysuomi: Added makefiles (for presidents' speeches).
    Added kotus/nykysuomi/Makefile:
      The main makefile for the subcorpora of the Modern Finnish corpus.
    
    Added kotus/nykysuomi/ns-common.mk:
      Common definitions for the subcorpora of the Modern Finnish corpus.
    
    Added kotus/nykysuomi/presidentti.mk
      Definitions for the presidents' speeches corpus.

---
+++ kotus/nykysuomi/ns-common.mk
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+CORPNAME_PREFIX = ns_
+
+MAKE_VRT_CMD = ../../scripts/tei2vrt.py $(TEI2VRT_OPTS)
+
+include ../../corp-common.mk
---
+++ kotus/nykysuomi/presidentti.mk
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+TEI2VRT_OPTS = --mode=words
+
+SRC_FILES = kekkonen_1957.xml
+
+P_ATTRS = lemma lemmacomp pos msd id
+S_ATTRS = text:0+distributor+source+title para:0+id+type+topic \
+		sentence:0+id+type
+
+include ns-common.mk
