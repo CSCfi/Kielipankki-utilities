43b83a61fec40a8506992726c93ccb2fc644bef0
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-08-20 14:18:28 +0300

    kotus/nykysuomi/saadokset.mk: Added again s-attribute para:topic.
    kotus/nykysuomi/saadokset.mk:
      Added again s-attribute para:topic.

--- kotus/nykysuomi/saadokset.mk
+++ kotus/nykysuomi/saadokset.mk
@@ -6,7 +6,7 @@ TEI2VRT_OPTS = --mode=statute-modern --words --lemgrams
 SRC_FILES = laki10.xml
 
 P_ATTRS = lemma lemmacomp pos msd id lex
-S_ATTRS = text:0+distributor+source+title div:1+id+type para:0+id+type \
+S_ATTRS = text:0+distributor+source+title div:1+id+type para:0+id+type+topic \
 		sentence:0+id+type
 
 include ns-common.mk
