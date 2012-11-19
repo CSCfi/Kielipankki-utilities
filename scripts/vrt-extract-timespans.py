a09700078b45ce0efd1476886fd78dca6dcf4602
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2012-11-19 17:16:50 +0200

    scripts/vrt-extract-timespans.py: Added --full-dates.
    scripts/vrt-extract-timespans.py:
      Added get_current_year(): Get current year.
      Added get_current_year2(): Get current year without century.
      Added get_current_century(): Get current century.
      TimeSpanExtractor:
        Added DATE_GRAN_RANGES: Date granularity ranges.
        _make_extract_patterns(): Add optional month and day if _opts.full_dates.
        _extract_time(): Call _fix_date() for th extracted date.
        Added _fix_date(): Fix the date: remove punctuation; full year. If the
          date is invalid, return an empty string.
        Added _fix_year(): Make a four-digit year.
        output_timespans(): Removed making four-digit year. Output the time twice
          to make a timespan.
      getopts(): Made the current year the default for --century-pivot. Added
        --full-dates: allow a month and a day in addition to a year.

--- scripts/vrt-extract-timespans.py
+++ scripts/vrt-extract-timespans.py
@@ -9,23 +9,43 @@ import re
 from optparse import OptionParser
 # collections.OrderedDict requires Python 2.7
 from collections import defaultdict, OrderedDict
+from datetime import date
+
+
+def get_current_year():
+    return date.today().year
+
+
+def get_current_year2():
+    return get_current_year() % 100
+
+
+def get_current_century():
+    return get_current_year() / 100
 
 
 class TimespanExtractor(object):
 
     DEFAULT_PATTERN = r'((?:1[0-9]|20)[0-9][0-9])'
     DEFAULT_PATTERN_2 = r'((?:1[0-9]|20)?[0-9][0-9])'
+    DATE_GRAN_RANGES = [(1000, get_current_year()), (1, 12), (1, 31),
+                        (0, 24), (0, 59), (0, 59)]
 
     def __init__(self, opts):
         self._opts = opts
         self._make_extract_patterns()
         self._make_excludes()
         self._time_tokencnt = defaultdict(int)
+        self._curr_century = str(get_current_century())
+        self._prev_century = str(get_current_century() - 1)
 
     def _make_extract_patterns(self):
         self._extract_patterns = defaultdict(list)
         default_pattern = (self.DEFAULT_PATTERN_2 if self._opts.two_digit_years
                            else self.DEFAULT_PATTERN)
+        if self._opts.full_dates:
+            default_pattern = (default_pattern[:-1]
+                               + ('(?:[-./][0-9][0-9]){0,2})'))
         if (self._opts.extract_pattern == [] and not self._opts.unknown
             and not self._opts.fixed):
             self._opts.extract_pattern.append('* * ' + default_pattern)
@@ -108,19 +128,40 @@ class TimespanExtractor(object):
                     continue
                 mo = re.search(pattern, attrs[attrname])
                 if mo:
-                    return mo.expand(templ)
+                    date = self._fix_date(mo.expand(templ))
+                    if date:
+                        return date
         return ''  
+
+    def _fix_date(self, datestr):
+        datestr = datestr.strip()
+        if self._opts.full_dates:
+            date_parts = [part for part in re.split(r'[ -./T]+', datestr)
+                          if part != '']
+            if self._opts.two_digit_years:
+                date_parts[0] = self._fix_year(date_parts[0])
+            for (partnr, part) in enumerate(date_parts):
+                gran_range = self.DATE_GRAN_RANGES[partnr]
+                if not gran_range[0] <= int(part) <= gran_range[1]:
+                    return ''
+                if len(part) == 1:
+                    date_parts[partnr] = '0' + part
+            return ''.join(date_parts)
+        elif self._opts.two_digit_years:
+            return self._fix_year(datestr)
+        else:
+            return datestr
+
+    def _fix_year(self, year):
+        if self._opts.two_digit_years and len(year) == 2:
+            return (self._prev_century if int(year) > self._opts.century_pivot
+                    else self._curr_century) + year
+        else:
+            return year
 
     def output_timespans(self):
         for (time, tokencnt) in sorted(self._time_tokencnt.iteritems()):
-            if self._opts.two_digit_years and len(time) == 2:
-                # FIXME: These should be based either on the current
-                # century or on an explicitly specified one.
-                if int(time) > self._opts.century_pivot:
-                    time = '19' + time
-                else:
-                    time = '20' + time
-            sys.stdout.write('\t'.join([time, str(tokencnt)]) + '\n')
+            sys.stdout.write('\t'.join([time, time, str(tokencnt)]) + '\n')
 
 
 def getopts():
@@ -132,8 +173,9 @@ def getopts():
     optparser.add_option('--two-digit-years', action='store_true',
                          default=False)
     optparser.add_option('--exclude', action='append', default=[])
-    # FIXME: The default could be based on the current year
-    optparser.add_option('--century-pivot', default='20')
+    optparser.add_option('--century-pivot', type='int',
+                         default=get_current_year2())
+    optparser.add_option('--full-dates', action='store_true', default=False)
     (opts, args) = optparser.parse_args()
     return (opts, args)
 
