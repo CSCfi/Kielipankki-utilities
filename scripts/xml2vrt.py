4f3905c1d941693a548c91e10905f797ea5f80dd
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2013-02-27 16:19:18 +0200

    scripts/xml2vrt.py: Parse rules. Added --python-rule-file.
    scripts/xml2vrt.py:
      getopts(): Added --python-rule-file. Exit with an error if neither --rule-file
        nor --python-rule-file is specified.
      get_rules(): Parse the rule file specified in opts.rule_file or evaluate the
        rule file specified in opts.python_rule_file.

--- scripts/xml2vrt.py
+++ scripts/xml2vrt.py
@@ -8,6 +8,7 @@ import re
 
 from optparse import OptionParser
 
+from xml2vrt.rule_parse import ElemRuleParser
 from xml2vrt.util import WrappedXMLFileReader
 from xml2vrt.converter import Converter, _test_rules
 # This is needed only as long as we need to be able to evaluate rules
@@ -18,9 +19,14 @@ from xml2vrt.rule_ast import *
 def getopts():
     optparser = OptionParser()
     optparser.add_option('--rule-file', '--rules', '-r')
+    optparser.add_option('--python-rule-file')
     optparser.add_option('--wrapper-elem', '--wrapper-element-name',
                          default=None)
     (opts, args) = optparser.parse_args()
+    if not opts.rule_file and not opts.python_rule_file:
+        sys.stderr.write('Please specify a rule file with --rule-file or '
+                         '--python-rule-file\n')
+        exit(1)
     if opts.wrapper_elem == '':
         opts.wrapper_elem = '__DUMMY__'
     return (opts, args)
@@ -28,10 +34,10 @@ def getopts():
 
 def get_rules(opts):
     if opts.rule_file:
-        rules = eval(read_file(opts.rule_file))
-    else:
-        rules = _test_rules
-    return rules
+        parser = ElemRuleParser()
+        return parser.parse(read_file(opts.rule_file))
+    elif opts.python_rule_file:
+        return eval(read_file(opts.python_rule_file))
 
 
 def read_file(fname):
