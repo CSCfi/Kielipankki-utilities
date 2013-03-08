dfeb587ff0be9b6604f13bc45998e85d348d59b7
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2013-03-08 11:00:00 +0200

    ftb/ftb2.mk: Excluded source file sofie* (Sophie's World).

--- ftb/ftb2.mk
+++ ftb/ftb2.mk
@@ -5,7 +5,7 @@ P_ATTRS = lemma pos msd dephead deprel lex
 S_ATTRS = sentence:0+id subcorpus:0+name
 
 SRC_FILES = *_tab.txt
-SRC_FILES_EXCLUDE = news-samples%
+SRC_FILES_EXCLUDE = news-samples% sofie%
 
 MAKE_VRT_CMD = ./ftb2vrt.pl --lemgrams --no-fix-morpho-tags # --morpho-tag-separator=":"
 # ftb2vrt.pl needs file names as arguments; does not read from stdin.
