1ec256081c474d2c76bc0677c129778c3e21109f
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2013-04-03 20:23:29 +0300

    scripts/ftb3-augment-loc.py: --source-type. Support JRC Acquis.
    scripts/ftb3-augment-loc.py:
      OrigFileReader:
        Added class attribute _clear_previous_info.
        __init__(): Added attribute _dir_last_part.
        get_info_for_line(): Changed parameter linenum to linenums (a list). Honour
          _clear_previous_info.
        _make_filename(): Remove from the filename the parts up to _dir_last_part.
        _get_tag_attr(): Match in the middle of a line.
        Added _get_elem_content().
      OrigFileReaderEuroparl._fill_info_from_line(): Changed "speaker" to "speech".
      Added class OrigFileReaderJRCAcquis as a subclass of OrigFileReader.
      LocAugmenter:
        Added class attributes _origfile_reader_class, _add_attrs.
        __init__(): Use _origfile_reader_class.
        _make_info_line(): Allow different spacing in <loc> line. Allow multiple
          line numbers.
        Added _get_linenums().
        _add_info(): Use _add_attrs with _opts.source_type as key.
      getopts(): Added --source-type=europarl|jrc.

--- scripts/ftb3-augment-loc.py
+++ scripts/ftb3-augment-loc.py
@@ -5,23 +5,29 @@
 import sys
 import re
 import codecs
+import os.path
 
 from optparse import OptionParser
 
 
 class OrigFileReader(object):
 
+    _clear_previous_info = False
+
     def __init__(self, directory=None, encoding='utf-8'):
         self._directory = directory
         self._encoding = encoding
+        self._dir_last_part = os.path.split(directory)[1]
         self._fname = None
         self._file = None
         self._linenum = 0
         self._info = {}
 
-    def get_info_for_line(self, fname, linenum):
+    def get_info_for_line(self, fname, linenums):
         self._open_if_new_file(fname)
-        while self._linenum < linenum:
+        if self._clear_previous_info and self._linenum < linenums[-1]:
+            self._info = {}
+        while self._linenum < linenums[-1]:
             line = self._file.readline()
             self._linenum += 1
             self._fill_info_from_line(line)
@@ -37,14 +43,27 @@ class OrigFileReader(object):
             self._linenum = 0
 
     def _make_filename(self, fname):
-        return (self._directory + '/' if self._directory else '') + fname
+        (fname_dir, fname) = os.path.split(fname)
+        fname_parts = [fname]
+        while fname_dir:
+            (fname_dir, fname_dir_last_part) = os.path.split(fname_dir)
+            if fname_dir_last_part == self._dir_last_part:
+                break
+            fname_parts[0:0] = [fname_dir_last_part]
+        fname_parts[0:0] = [self._directory]
+        return os.path.join(*fname_parts)
 
     def _fill_info_from_line(self, line):
         pass
 
     def _get_tag_attr(self, line, attrname, default=None):
-        mo = re.match(r'<.+\s' + attrname + r'=([^"]+?|".*?")[\s>]', line)
+        mo = re.search(r'<.+\s' + attrname + r'=([^"]+?|".*?")[\s>]', line)
         return mo.group(1).strip('"') if mo else default
+
+    def _get_elem_content(self, line, elemname, default=None):
+        mo = re.search(r'<' + elemname + r'(?:\s.*?)?>(.*?)</' + elemname + '>',
+                       line)
+        return mo.group(1) if mo else default
 
 
 class OrigFileReaderEuroparl(OrigFileReader):
@@ -59,7 +78,7 @@ class OrigFileReaderEuroparl(OrigFileReader):
             self._info['chapter_id'] = self._get_tag_attr(line, 'ID')
             self._info['p_id'] = '1'
         elif line.startswith('<SPEAKER'):
-            self._info['type'] = 'speaker'
+            self._info['type'] = 'speech'
             self._info['speaker_id'] = self._get_tag_attr(line, 'ID')
             self._info['speaker_name'] = self._get_tag_attr(line, 'NAME')
             self._info['language'] = (self._get_tag_attr(line, 'LANGUAGE')
@@ -68,11 +87,53 @@ class OrigFileReaderEuroparl(OrigFileReader):
             self._info['p_id'] = str(int(self._info['p_id']) + 1)
 
 
+class OrigFileReaderJRCAcquis(OrigFileReader):
+
+    _clear_previous_info = True
+
+    def __init__(self, directory=None, encoding='utf-8'):
+        OrigFileReader.__init__(self, directory, encoding)
+
+    def _fill_info_from_line(self, line):
+        line = line.strip()
+        if line.startswith('<teiHeader '):
+            self._info = {'type': 'title'}
+        elif line.startswith('<title>'):
+            title = self._get_elem_content(line, 'title')
+            title_key = 'title' if 'title0' in self._info else 'title0'
+            self._info[title_key] = title
+        elif line.startswith('<bibl>'):
+            url = self._get_elem_content(line, 'xref')
+            # The URLs in the files have the older domain europa.eu.int
+            url = url.replace('europa.eu.int', 'europa.eu')
+            self._info['url'] = url
+        elif line.startswith('<p '):
+            p_id = self._get_tag_attr(line, 'n')
+            if 'type' not in self._info:
+                self._info['type'] = 'p'
+            if 'p_id' not in self._info:
+                self._info['p_id'] = p_id
+            else:
+                self._info['p_id'] += ',' + p_id
+
+
 class LocAugmenter(object):
+
+    _origfile_reader_class = {'europarl': OrigFileReaderEuroparl,
+                              'jrc': OrigFileReaderJRCAcquis}
+
+    _add_attrs = {'europarl':
+                      {'chaptertitle': ['type', 'chapter_id'],
+                       'speech': ['type', 'speaker_id', 'language',
+                                  'speaker_name', 'p_id']},
+                  'jrc':
+                      {'title': ['title', 'title0', 'url', 'p_id'],
+                       'p': ['p_id']}
+                  }
 
     def __init__(self, opts):
         self._opts = opts
-        self._orig_reader = OrigFileReaderEuroparl(
+        self._orig_reader = self._origfile_reader_class[self._opts.source_type](
             directory=self._opts.original_file_directory)
 
     def print_augmented(self, files):
@@ -104,21 +165,23 @@ class LocAugmenter(object):
                 yield line
 
     def _make_info_line(self, line):
-        mo = re.match(r'<s><loc file=".+/([^/]+)" line="(.*?)"/>', line)
+        mo = re.match(r'<s>\s*<loc\s+file="(.+?)"\s+line="(.*?)"\s*/>', line)
         (fname, linenum) = (mo.group(1), mo.group(2))
-        info = self._orig_reader.get_info_for_line(fname, int(linenum))
+        info = self._orig_reader.get_info_for_line(fname,
+                                                   self._get_linenums(linenum))
         # print fname, linenum, info
         if self._opts.loc_only:
             line = line[3:]
         return self._add_info(line, info)
 
+    def _get_linenums(self, linenum_str):
+        return [int(linenum) for linenum in linenum_str.split(',')]
+
     def _add_info(self, line, info):
-        add_attrs = {'chaptertitle': ['type', 'chapter_id'],
-                     'speaker': ['type', 'speaker_id', 'language',
-                                 'speaker_name', 'p_id']}
-        return (line.rstrip('/>\n') + ' '
-                + self._make_attributes(add_attrs[info['type']], info)
-                + '/>\n')
+        return (line.rstrip(' />\n') + ' '
+                + self._make_attributes(
+                self._add_attrs[self._opts.source_type][info['type']], info)
+                + ' />\n')
 
     def _make_attributes(self, names, valuedict):
         return ' '.join([u'{name}="{val}"'.format(name=name,
@@ -137,6 +200,11 @@ information available in the original files."""
     optparser.add_option('--loc-only', action='store_true',
                          help=('Output only the augmented loc elements, '
                                'not all the input'))
+    optparser.add_option('--source-type', type='choice',
+                         choices=['europarl', 'jrc'], default='europarl',
+                         help=('Use the source type TYPE: europarl for'
+                               ' EuroParl, jrc for JRC Acquis'
+                               ' (default: %default)'), metavar='TYPE')
     (opts, args) = optparser.parse_args()
     return (opts, args)
 
