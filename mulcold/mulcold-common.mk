33d733eb51de40046760aa5835a7da37f4d95c46
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2013-11-13 16:59:07 +0200

    Makefiles for MULCOLD parallel corpus test files
    Added mulcold/Makefile,
    Added mulcold/mulcold-common.mk,
    Added mulcold/mulcold_fiensvru.mk,
    Added mulcold/mulcold_fiensvru_any.mk,
    Added mulcold/mulcold_firu.mk,
    Added mulcold/mulcold_firu_any.mk:
      Makefiles for MULCOLD (Multilingual Corpus of Legal Documents),
        Finnish-English-Swedish-Russian and Finnish-Russian parallel corpora

---
+++ mulcold/mulcold-common.mk
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+P_ATTRS = lemma pos msd
+
+VRT_FIX_ATTRS_OPTS_EXTRA = --missing-field-values="3:UNKNOWN"
+
+include ../corp-common.mk
---
+++ mulcold/mulcold_firu_any.mk
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+SRC_SUBDIR = mulcold/firu
+
+SRC_FILES = *_$(PARCORP_LANG).vrt
+
+include mulcold-common.mk
