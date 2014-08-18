4f14c4e86216f31c3e726efb7cd571f44218e7fa
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2014-08-18 12:52:43 +0300

    tei2vrt.py: Handle Kotus metadata, Kaino links
    scripts/tei2vrt.py:
      Handle Kotus RDF metadata files (--kotus-metadata).
      Support adding links to fulltext context in Kaino (--kaino-urls).
      Added --filename: specify the name of the input file if read from stdin.
      Some extra attributes for the Kotus corpus of presidents' speeches.
      Converter:
        process_input(): Set attribute _url_base. Call _read_metadata().
        Added _make_url_base(), _read_metadata(), _make_metadata_filename().
        _add_text_info(): If metadata, add the following attributes to element
          "text": author, author_birthyear, author_deathyear, date, url,
          original_url.
        Added _get_author_info(), _get_coverage(), _get_external_link().
        _process_body_elem(): Added sub-function _extract_topic(). Separate
          topic and span attributes for a paragraph. Add attribute url if
          a base url has been specified.
      getopts(): Added --kaino-urls, --kotus-metadata, --filename.

--- scripts/tei2vrt.py
+++ scripts/tei2vrt.py
@@ -20,8 +20,47 @@ class Converter(object):
 
     def process_input(self, f):
         self._src_etr = et.parse(f)
+        self._url_base = self._make_url_base()
+        self._read_metadata(f)
         result_etr = self._convert()
         result_etr.write(sys.stdout, encoding='utf-8')
+
+    def _make_url_base(self):
+        if self._opts.kaino_urls:
+            sourcedesc = self._get_source()
+            # TODO: Make this more general
+            return ('http://kaino.kotus.fi'
+                    + (sourcedesc.replace('/meta/', '/teksti/')
+                       .replace('_rdf.xml', '.xml')))
+        else:
+            return None
+
+    def _read_metadata(self, fname):
+        if self._opts.kotus_metadata:
+            metadata_fname = self._make_metadata_filename(fname)
+            # register_namespace does not seem to have an effect, so
+            # make a namespace dict of our own
+            self._metadata_namespaces = {
+                'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+                'dc': 'http://purl.org/dc/elements/1.1/',
+                'dcterms': 'http://purl.org/dc/terms/',
+                'kotus': 'http://www.kotus.fi/'}
+            for prefix, uri in self._metadata_namespaces.iteritems():
+                et.register_namespace(prefix, uri)
+            self._metadata_etr = et.parse(metadata_fname)
+        else:
+            self._metadata_etr = None
+
+    def _make_metadata_filename(self, f):
+        if isinstance(f, basestring):
+            fname = f
+        else:
+            fname = self._opts.filename or getattr(f, 'name', '')
+        # FIXME: This is specific to Kotus corpora
+        fname_parts = fname.partition('/teksti/')
+        metadata_fname_parts = self._get_source().partition('/meta/')
+        return (fname_parts[0] + metadata_fname_parts[1]
+                + metadata_fname_parts[2])
 
     def _convert(self):
         text_e = et.Element("text")
@@ -34,6 +73,15 @@ class Converter(object):
         text_e.set("title", self._get_title())
         text_e.set("distributor", self._get_distributor())
         text_e.set("source", self._get_source())
+        if self._metadata_etr:
+            author, author_birthyear, author_deathyear = self._get_author_info()
+            text_e.set("author", author)
+            text_e.set("author_birthyear", author_birthyear)
+            text_e.set("author_deathyear", author_deathyear)
+            text_e.set("date", self._get_coverage())
+            # FIXME: Make this more general
+            text_e.set("url", 'http://kaino.kotus.fi' + self._get_source())
+            text_e.set("original_url", self._get_external_link())
 
     def _get_title(self):
         return (self._src_etr.find("teiHeader//title").text.strip()
@@ -48,6 +96,29 @@ class Converter(object):
 
     def _get_source(self):
         return self._src_etr.find("teiHeader//sourceDesc/p").text.strip()
+
+    def _get_author_info(self):
+        creator_info = self._metadata_etr.find(
+            './/dc:creator', namespaces=self._metadata_namespaces).text.strip()
+        name_mo = re.match(r'(.+?)(?: \(|$)', creator_info)
+        name = name_mo.group(1) if name_mo else ''
+        years_mo = re.search(r'\((\d+)(?:\D+)(\d*)\)', creator_info)
+        birth_year, death_year = years_mo.group(1, 2) if years_mo else ('', '')
+        return name, birth_year, death_year
+
+    def _get_coverage(self):
+        return self._metadata_etr.find(
+            './/dc:coverage', namespaces=self._metadata_namespaces).text.strip()
+
+    def _get_external_link(self):
+        elem = self._metadata_etr.find('.//*[@kotus:type="external_link"]',
+                                       namespaces=self._metadata_namespaces)
+        if elem is not None:
+            return (elem.get('{' + self._metadata_namespaces['rdf'] + '}'
+                             + 'resource', '')
+                    or '')
+        else:
+            return ''
 
     def _add_body(self, text_e):
         body_roots = self._src_etr.findall(".//text/body")
@@ -64,7 +135,11 @@ class Converter(object):
                 result.text += subresults
             else:
                 result.extend(subresults)
-            
+
+        def _extract_topic(text, key):
+            mo = re.search(r' ' + key + '/(\S+)', text)
+            return mo.group(1) if mo else ''
+
         result = None
         id_attr = body_e.get('id', '')
         # print '<' + body_e.tag + " id=" + id_attr
@@ -83,7 +158,9 @@ class Converter(object):
                 result = et.Element(self._opts.para_elem_name)
                 _process_subelems(result)
             if body_e.get('type'):
-                result.set('topic', body_e.get('type'))
+                topic_and_span = body_e.get('type')
+                result.set('topic', _extract_topic(topic_and_span, 'TOPIC'))
+                result.set('span', _extract_topic(topic_and_span, 'SPAN'))
             result.set('type', body_e.tag)
         elif body_e.tag == 'div':
             type_ = body_e.get('type', '')
@@ -110,6 +187,8 @@ class Converter(object):
             result = et.Element(body_e.tag)
             _process_subelems(result)
         result.set('id', id_attr)
+        if self._url_base:
+            result.set('url', self._url_base + '#' + id_attr)
         self._add_content_newlines(result)
         # print "<<" + result.text + ">>"
         # print '>' + body_e.tag + " id=" + id_attr + " " + result.get('id')
@@ -385,6 +464,10 @@ def getopts():
     optparser.add_option('--no-fix-msd-tags', '--no-fix-morpho-tags',
                          action='store_false', dest='fix_msd_tags',
                          default=True)
+    # FIXME: Make the following two option names more general
+    optparser.add_option('--kaino-urls', action='store_true')
+    optparser.add_option('--kotus-metadata', action='store_true')
+    optparser.add_option('--filename')
     (opts, args) = optparser.parse_args()
     return (opts, args)
 
