1b3b8c50c86c6cf4fc03b0cf1dff9233662f41f2
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2014-11-15 17:00:00 +0200

    sbatch-extract-rels: Subdirectories; --dry-run
    scripts/sbatch-extract-rels.sh:
      Put input, output and log files in subdirectories.
      Added --dry-run to show the sbatch script.
      Adjusted the requested cpu time and memory allocation: doubled run time
        and maximum memory.
    
    (Commit author date backdated to about the original modification date.)

--- scripts/sbatch-extract-rels.sh
+++ scripts/sbatch-extract-rels.sh
@@ -2,8 +2,14 @@
 
 scriptdir=`dirname $0`
 
-ls -lS klk_fi*.tgz |
+ls -lS in/klk_fi*.tgz |
 gawk '{printf "%2d\t%s\n", log($5)/log(2)+1, gensub("[^0-9]+", "", "g", $8)}' > all_years.txt
+
+action=sbatch
+if [ "x$1" = "x--dry-run" ] || [ "x$1" = "x-n" ]; then
+    action=cat
+    shift
+fi
 
 if [ "x$1" != x ]; then
     sizes="$*"
@@ -14,7 +20,7 @@ fi
 for size in $sizes; do
     rm -f years_$size.txt
     for year in `egrep "^$size" all_years.txt | cut -f2`; do
-	if [ ! -e $scriptdir/klk_fi_${year}_rels.tar ]; then
+	if [ ! -e $scriptdir/out/klk_fi_${year}_rels.tar ]; then
 	    echo $year >> years_$size.txt
 	fi
     done
@@ -24,13 +30,13 @@ for size in $sizes; do
     if [ -s years_$size.txt ]; then
 	name=rels_$size
 	num=`wc -l < years_$size.txt`
-	mins=`gawk 'BEGIN {a = 2 ** ('$size' - 20) / 20 + 1; printf "%d", a}'`
-	mem=`gawk 'BEGIN {a = 2 ** ('$size' - 15); if (a < 128) {a = 128} if (a > 64000) {a = 64000}; print a}'`
-	sbatch <<EOF
-#! /bin/bash
+	mins=`gawk 'BEGIN {a = 2 ** ('$size' - 20) / 10 + 1; printf "%d", a}'`
+	mem=`gawk 'BEGIN {a = 2 ** ('$size' - 15); if (a < 128) {a = 128} if (a > 128000) {a = 128000}; print a}'`
+	$action <<EOF
+#! /bin/bash -l
 #SBATCH -J $name
-#SBATCH -o ${name}_%A_%a.out
-#SBATCH -e ${name}_%A_%a.err
+#SBATCH -o log/${name}_%A_%a.out
+#SBATCH -e log/${name}_%A_%a.err
 #SBATCH -t $mins
 #SBATCH --mem-per-cpu $mem
 #SBATCH --array=1-$num
@@ -39,6 +45,7 @@ for size in $sizes; do
 
 cd $scriptdir
 year=\$(sed -n "\$SLURM_ARRAY_TASK_ID"p years_$size.txt)
+echo Job: $SLURM_JOB_ID $SLURM_JOB_NAME
 ./run-extract-rels.sh \$year
 EOF
     fi
