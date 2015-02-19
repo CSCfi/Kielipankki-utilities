a4c98e17c48ab5ab1a7eae179f2310b821155ec4
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2015-02-19 12:27:17 +0200

    korp-install-corpora: Pkgs in pkg dir; opts; etc
    scripts/korp-install-corpora.sh:
      A major ovehaul of the script.
      Choose a default corpus root among directories listed.
      Install packages from a (default or specified) package directory or its
        subdirectories, instead of the current directory.
      Install packages from a remote directory via SSH, without having to make
        local copies.
      Specify either package file names or corpus names; if none specified,
        abort instead of defaulting to all non-installed corpora.
      Install only the latest matching corpus package if newer than the
        currently installed one.
      Log installed corpora to a global log file.
      Add general script infrastructure: functions warn, error, cleanup,
        cleanup_abort; usage message; option processing.

--- scripts/korp-install-corpora.sh
+++ scripts/korp-install-corpora.sh
@@ -1,19 +1,20 @@
 #! /bin/sh
+# -*- coding: utf-8 -*-
 
 # Install Korp corpora of the latest corpora package
 
 # TODO:
-# - If several packages for the same corpus exist in the same
-#   directory, install only the latest one.
 # - Backup (optionally?) the previous version of corpus data and
 #   registry files, or at least warn if the corpus exists.
-# - Options and a usage message.
+# - Option to force installing a package file older than the currently
+#   installed version
+# - Verbosity control
+# - Ignore tar errors of missing sql directory
 
 
-# Corpus package directory
-pkgdir=${CORPUS_PKGDIR:-.}
-# Root directory, relative to which the corpus directory resides
-rootdir=${ROOTDIR:-/v/corpora}
+progname=`basename $0`
+progdir=`dirname $0`
+
 
 # Korp MySQL database
 korpdb=korp
@@ -28,56 +29,274 @@ if [ "x$KORP_MYSQL_PASSWORD" != "x" ]; then
     mysql_opts="$mysql_opts --password=$KORP_MYSQL_PASSWORD"
 fi
 
-scriptname=`basename $0`
-scriptdir=`dirname $0`
+default_corpus_roots="/v/corpora /proj/clarin/korp/corpora $WRKDIR/corpora /wrk/jyniemi/corpora"
+find_existing_dir () {
+    _test=$1
+    _file=$2
+    shift 2
+    for dir in "$@"; do
+	if [ $_test $dir/$_file ]; then
+	    echo $dir
+	    break
+	fi
+    done
+}
 
+# Root directory, relative to which the corpus directory resides
+corpus_root=${CORPUS_ROOT:-$(find_existing_dir -d "" $default_corpus_roots)}
+
+pkgsubdir=pkgs
+
+# This will be set later based on $corpus_root, which may be modified
+# by options
+pkgdir=$CORPUS_PKGDIR
+
+# This is only for compatibility with older corpus packages
 pkg_prefix=korpdata_
 
-timestampfile=$pkgdir/korp_fi_latest_corpus_packages_newer.timestamp
-installed_list=$pkgdir/korp_fi_installed_corpora.list
+tmpdir=${TMPDIR:-${TEMPDIR:-${TMP:-${TEMP:-/tmp}}}}
+tmp_prefix=$tmpdir/$progname
 
-tmpdir=${tmpdir:-${TMPDIR:-$pkgdir}}
-filelistfile=$tmpdir/$scriptname.files.$$
+filelistfile=$tmp_prefix.files.$$
+pkglistfile=$tmp_prefix.pkgs.$$
+
+timestamp_format="+%Y-%m-%dT%H:%M:%S"
+
+
+warn () {
+    echo "$progname: Warning: $1" >&2
+}
+
+error () {
+    echo "$progname: $1" >&2
+    exit 1
+}
+
+cleanup () {
+    if [ "x$tmp_prefix" != "x" ]; then
+	rm -rf $tmp_prefix.*
+    fi
+}
+
+cleanup_abort () {
+    cleanup
+    exit 1
+}
+
+
+trap cleanup 0
+trap cleanup_abort 1 2 13 15
+
+
+usage () {
+    cat <<EOF
+Usage: $progname [options] corpus|package ...
+
+Install or update the specified corpora from corpus packages to Corpus
+Workbench and Korp database.
+
+The arguments may be either corpus package file names, possibly with a full
+path name, or corpus names, in which case packages for the corpus are searched
+for in the package directory. A full path name may be preceded by a remote
+host name and a colon, in which case the package is retrieved from the remote
+host without a need to make a local copy of the package file.
+
+Options:
+  -h, --help      show this help
+  -c, --corpus-root DIR
+                  use DIR as the root directory of corpus files (default:
+                  $corpus_root)
+  -p, --package-dir DIR
+                  DIR is the default directory in which to search for corpus
+                  packages to be installed; may be a remote directory
+                  specified as HOST:DIR (default: CORPUS_ROOT/$pkgsubdir)
+EOF
+    exit 0
+}
+
+
+# Test if GNU getopt
+getopt -T > /dev/null
+if [ $? -eq 4 ]; then
+    # This requires GNU getopt
+    args=`getopt -o "hcp:r:" -l "help,corpus-root:,package-dir:" -n "$progname" -- "$@"`
+    if [ $? -ne 0 ]; then
+	exit 1
+    fi
+    eval set -- "$args"
+fi
+# If not GNU getopt, arguments of long options must be separated from
+# the option string by a space; getopt allows an equals sign.
+
+# Process options
+while [ "x$1" != "x" ] ; do
+    case "$1" in
+	-h | --help )
+	    usage
+	    ;;
+	-c | --corpus-root )
+	    corpus_root=$2
+	    shift
+	    ;;
+	-p | --package-dir )
+	    pkgdir=$2
+	    shift
+	    ;;
+	-- )
+	    shift
+	    break
+	    ;;
+	--* )
+	    warn "Unrecognized option: $1"
+	    ;;
+	* )
+	    break
+	    ;;
+    esac
+    shift
+done
+
+if [ "x$1" = x ]; then
+    error "Please specify the names of corpus packages or corpora to install.
+For more information, run '$0 --help'."
+fi
+
+pkgdir=${pkgdir:-$corpus_root/$pkgsubdir}
+
+default_pkghost=-
+case $pkgdir in
+    *:* )
+	default_pkghost=${pkgdir%%:*}
+	pkgdir=${pkgdir#*:}
+	;;
+esac
+
+installed_list=$corpus_root/korp_installed_corpora.list
 
 if [ ! -e "$installed_list" ]; then
     touch "$installed_list"
 fi
 
-get_pkg_fname () {
-    corp=$1
-    pkg_fnames=`for base in $pkg_prefix$corp ${corp}_korp_20\*; do echo "$pkgdir/$base.t?z $pkgdir/$base.tar*"; done`
-    ls -t $pkg_fnames 2> /dev/null | head -1 | awk '{print $NF}'
+
+get_package_corpus_name () {
+    corpname=${1##*/}
+    corpname=${corpname%.t?z}
+    corpname=${corpname%.tar.*}
+    case "$1" in
+	"$pkgprefix*" )
+            corpname=${corpname##$pkgprefix}
+            ;;
+	* )
+	    corpname=${corpname%_korp_20*}
+	    ;;
+    esac
+    echo "$corpname"
 }
 
-if [ "x$1" != "x" ]; then
-    corpora=
-    for corp in "$@"; do
-	corpus_pkg=`get_pkg_fname $corp`
-	if [ "x$corpus_pkg" = "x" ]; then
-	    echo "Cannot read a package file for corpus $corp; ignoring" \
-		> /dev/stderr
+find_corpus_packages () {
+    pkgspec=$1
+    pkghost=$default_pkghost
+    use_find=1
+    current_pkgdir=$pkgdir
+    case $pkgspec in
+	*:* )
+	    pkghost=${pkgspec%%:*}
+	    pkgspec=${pkgspec#*:}
+	    ;;
+    esac
+    # TODO: If $pkgspec is a directory, find packages in it and its
+    # subdirectories
+    case $pkgspec in
+	*/*.* )
+	    use_find=
+	    ;;
+	*/* )
+	    current_pkgdir=$pkgspec
+	    pkgname_cond="-name \*_korp_20\*.t?z -o -name \*_korp_20\*.tar.\* -o -name $pkg_prefix\*.t?z -o -name $pkg_prefix\*.tar.*"
+	    ;;
+	*.* )
+	    pkgname_cond="-name $pkgspec"
+	    ;;
+	* )
+	    pkgname_cond="-name ${pkgspec}_korp_20\*.t?z -o -name ${pkgspec}_korp_20\*.tar.\* -o -name $pkg_prefix$pkgspec.t?z -o -name $pkg_prefix$pkgspec.tar.*"
+	    ;;
+    esac
+    ls_cmd="ls -lt --time-style=$timestamp_format --dereference"
+    if [ "x$use_find" = x ]; then
+	cmd="$ls_cmd $pkgdir/$pkgspec $pkgspec 2> /dev/null"
+    else
+	cmd="find $current_pkgdir $maxdepth_opt $pkgname_cond | xargs --no-run-if-empty $ls_cmd"
+    fi
+    if [ "$pkghost" != - ]; then
+	ssh $pkghost "$cmd"
+    else
+	eval "$cmd"
+    fi |
+    while read mode links owner group size timestamp pkgname; do
+	printf "%s\t%s\t%s\t%s\t%s\n" $(get_package_corpus_name $pkgname) \
+	     $pkghost "$pkgname" $timestamp $size
+    done
+}
+
+find_package_candidates () {
+    listfile=$1
+    shift
+    touch $listfile
+    for pkgspec in "$@"; do
+	find_corpus_packages "$pkgspec" > $listfile.tmp
+	if [ -s $listfile.tmp ]; then
+	    grep -Fvx -f$listfile < $listfile.tmp >> $listfile
 	else
-	    corpora="$corpora $corp"
+	    warn "No matching corpus packages found: $pkgspec"
 	fi
     done
-else
-    if [ -e "$timestampfile" ]; then
-	newer_cond="-newer $timestampfile"
-    else
-	echo "Timestamp file $timestampfile not found; considering all non-installed corpus packages in $pkgdir" > /dev/stderr
-	newer_cond=
-    fi
-    corpora=`find $pkgdir -name $pkg_prefix\*.t?z -o -name $pkg_prefix\*.tar.* -o -name \*_korp_20\*.t?z -o -name \*_korp_20\*.tar.* $newer_cond \
-	| sed -e "s/^.*$pkg_prefix//; s/^.*\/\(.*\)_korp_20.*/\1/; s/\.t.z//; s/\.tar\..*//" \
-	| grep -Fvx -f$installed_list \
-	| sort`
-fi
-if [ "x$corpora" = "x" ]; then
-    echo "$scriptname: No corpora to install!"
-    exit 1
-fi
+}
 
-trap "echo Aborting installation; rm -f $filelistfile; exit 1" HUP INT QUIT
+filter_corpora () {
+    listfile=$1
+    corpname_prev=
+    corp_pkgfile=
+    corpora_to_install=
+    # The following cannot be a pipeline because the values of the
+    # variables would not be retained.
+    sort -t"	" -s -k1,1 -k4,4r $listfile > $listfile.srt
+    while read corpname pkghost pkgfile timestamp pkgsize; do
+	if [ "x$corpname" = "x$corpname_prev" ]; then
+	    :
+	    # TODO: Show this only with --verbose
+	    # echo "  $corpname: skipping $pkgfile as older than $corp_pkgfile" > /dev/stderr
+	else
+	    installed_date=$(grep -E "^[^	]+	$corpname	" $installed_list \
+		| cut -d'	' -f4 \
+		| sort -r \
+		| head -1)
+	    if expr "$timestamp" "<=" "$installed_date" > /dev/null; then
+		echo "  $corpname: skipping $pkgfile as not newer than the installed package ($timestamp <= $installed_date)" > /dev/stderr
+	    else
+		printf "%s\t%s\t%s\t%s\t%s\n" $corpname $pkghost "$pkgfile" \
+		    $timestamp $pkgsize
+		corpora_to_install="$corpora_to_install $corpname"
+	    fi
+	    corpname_prev=$corpname
+	    corp_pkgfile=$pkgfile
+	fi
+    done < $listfile.srt
+}
+
+get_tar_compress_opt () {
+    case "$1" in
+	*.gz | *.tgz )
+	    echo --gzip
+	    ;;
+	*.tbz | *.bz | *.bz2 )
+	    echo --bzip2
+	    ;;
+	*.xz | *.txz )
+	    echo --xz
+	    ;;
+    esac
+}
+
 
 # Cat possibly compressed file(s) based on the file name extension
 comprcat () {
@@ -97,9 +316,8 @@ comprcat () {
     esac
 }
 
-# Return the size of a file in a human-readable format
-filesize () {
-    kb=`du -ks $1 | cut -f1`
+human_readable_size () {
+    kb=$(($1 / 1024))
     if [ "$kb" -lt 10240 ]; then
 	echo "$kb KiB"
     else
@@ -107,29 +325,40 @@ filesize () {
     fi
 }
 
+# Return the size of a file in a human-readable format
+filesize () {
+    human_readable_size $(du -bs $1 | cut -f1)
+}
+
 adjust_registry () {
+    # FIXME: This probably does not work correctly if datadir is other
+    # than $corpus_root/data/$corpus_id
     filelistfile=$1
-    regfile=$rootdir/`grep registry/ $filelistfile`
-    if ! grep -E -ql $rootdir $regfile 2> /dev/null; then
-	datadir=$rootdir/`grep -E '/\.info$' $filelistfile | sed -e 's,/.info,,'`
-	mv $regfile $regfile.orig
-	sed -e "s,^\(HOME\|INFO\) .*$corpus_id\(.*\),\1 $datadir\2," \
-	    $regfile.orig > $regfile
-	touch --reference=$regfile.orig $regfile
-	rm $regfile.orig
-    fi
+    grep registry/ $filelistfile |
+    while read regfile_base; do
+	regfile=$corpus_root/$regfile_base
+	corpus_id=${regfile_base##*/}
+	datadir=$(echo "$regfile" | sed -e 's,/registry/,/data/,')
+	if ! grep -E -ql $corpus_root $regfile 2> /dev/null; then
+	    mv $regfile $regfile.orig
+	    sed -e "s,^\(HOME\|INFO\) .*/$corpus_id\(/[^/]*\)\?,\1 $datadir\2," \
+		$regfile.orig > $regfile
+	    touch --reference=$regfile.orig $regfile
+	    rm $regfile.orig
+	fi
+    done
 }
 
 install_file_sql () {
     sqlfile=$1
-    comprcat $rootdir/$sqlfile \
+    comprcat $sqlfile \
 	| mysql $mysql_opts $korpdb
     return $?
 }
 
 install_file_tsv () {
     tsvfile=$1
-    $scriptdir/korp-mysql-import.sh --prepare-tables $tsvfile
+    $progdir/korp-mysql-import.sh --prepare-tables $tsvfile
     return $?
 }
 
@@ -141,11 +370,10 @@ install_dbfiles () {
     if [ "x$files" != "x" ]; then
 	echo "  $msg data into MySQL database"
 	for file in $files; do
-	    echo "    $file (size `filesize $rootdir/$file`)"
-	    install_file_$type "$rootdir/$file"
+	    echo "    $file (size `filesize $corpus_root/$file`)"
+	    install_file_$type "$corpus_root/$file"
 	    if [ $? -ne 0 ]; then
-		echo "$scriptname: Errors in loading $file"
-		exit 1
+		error "Errors in loading $file"
 	    fi
 	done
     fi
@@ -157,35 +385,71 @@ install_db () {
     install_dbfiles tsv $filelistfile Importing
 }
 
+format_package_name_host () {
+    if [ "x$2" != x ]; then
+	printf $2:
+    fi
+    printf "%s\n" $1
+}
+
 install_corpus () {
-    corpus_pkg=`get_pkg_fname $corp`
+    corp=$1
+    corpus_pkg=$2
+    pkgtime=$3
+    pkgsize=$4
+    pkghost=$5
     echo
-    echo "Installing $corp (compressed size `filesize $corpus_pkg`)"
+    echo "Installing $corp from $(format_package_name_host $corpus_pkg $pkghost) (compressed size $(human_readable_size $pkgsize))"
     echo "  Copying CWB files"
-    tar xvap -C $rootdir -f $corpus_pkg --wildcards --wildcards-match-slash \
+    if [ "x$pkghost" = "x-" ] || [ "x$pkghost" = x ]; then
+	cat "$corpus_pkg"
+    else
+	ssh $pkghost "cat $corpus_pkg"
+    fi |
+    tar xvp -C $corpus_root -f- $(get_tar_compress_opt $pkgname) \
+	--wildcards --wildcards-match-slash \
 	--transform 's,.*/\(data\|registry\|sql\)/,\1/,' \
 	--show-transformed-names '*/data' '*/registry' '*/sql' 2>&1 \
 	| tee $filelistfile \
 	| sed -e 's/^/    /'
     if grep 'tar:' $filelistfile; then
-	echo "$scriptname: Errors in extracting $corpus_pkg"
-	exit 1
+	error "Errors in extracting $corpus_pkg"
     fi
     adjust_registry $filelistfile
     install_db $filelistfile
-    echo $corp >> $installed_list
+    # Log to the list of installed corpora: current time, corpus name,
+    # package file name, package file modification time, installing
+    # user
+    printf "%s\t%s\t%s\t%s\t%s\n" \
+	$(date "$timestamp_format") $corp $(basename "$corpus_pkg") \
+	$pkgtime $(whoami) >> $installed_list
 }
 
-echo Installing Korp corpora:
-for corp in $corpora; do
-  echo "  $corp"
-done
+install_corpora () {
+    echo
+    echo Installing Korp corpora:
+    for corp in $corpora_to_install; do
+	echo "  $corp"
+    done
+    while read corpname pkghost pkgfile pkgtime pkgsize; do
+	install_corpus $corpname "$pkgfile" $pkgtime $pkgsize $pkghost
+    done
+    echo
+    echo Installation complete
+}
 
-for corp in $corpora; do
-    install_corpus $corp
-done
 
-rm $filelistfile
+echo Searching for corpus packages to install
+find_package_candidates $pkglistfile.base "$@"
+if [ ! -s $pkglistfile.base ]; then
+    error "No matching corpus packages found"
+fi
 
-echo
-echo Installation complete
+# The following two cannot be in a pipeline, because the former
+# constructs the value of the variable corpora_to_install that the
+# latter uses
+filter_corpora $pkglistfile.base > $pkglistfile
+if [ "x$corpora_to_install" = x ]; then
+    error "No corpora to install"
+fi
+install_corpora < $pkglistfile
