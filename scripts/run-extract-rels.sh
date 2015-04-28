55ff06e3c1afe5b01084ec9ca73d29b013629d11
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2015-04-28 10:43:21 +0300

    run-extract-rels: Include word forms; fix tar
    scripts/run-extract-rels.sh:
      if --new, also produce relations with word forms (--include-word-forms).
      Full path for vrt-extract-relations.py.
      Change to the directory to package with tar, since --wildcards does not
        seem to work.

--- scripts/run-extract-rels.sh
+++ scripts/run-extract-rels.sh
@@ -4,7 +4,7 @@ date +'Start: %F %T %s.%N'
 
 opts=
 if [ "x$1" = "x--new" ]; then
-    opts="--output-type=new-strings"
+    opts="--output-type=new-strings --include-word-forms"
     shift
 fi
 
@@ -19,7 +19,7 @@ if [ ! -e $rels_tar ]; then
     mkdir -p out/$year
 
     tar xzOf in/klk_fi_${year}_parsed_vrt.tgz --wildcards $year/\*.vrt |
-    vrt-extract-relations.py \
+    /proj/clarin/korp/scripts/vrt-extract-relations.py \
 	--input-fields 'word lemma lemmacomp pos msd dephead deprel ref ocr lex' \
 	--relation-map in/wordpict_relmap_tdt.tsv \
 	--output-prefix out/$year/klk_fi_${year}_rels \
@@ -37,7 +37,12 @@ if [ ! -e $rels_tar ]; then
 	esac
 	sort $sort_opts $f.unsorted | gzip > $f.gz
     done
-    tar cpf $rels_tar -C out --wildcards $year/\*.gz
+    # tar cpf $rels_tar -C out --wildcards $year/\*.gz
+    # Wildcards do not seem to work in tar even with --wildcards. Why?
+    (
+	cd out
+	tar cpf ../$rels_tar --wildcards $year/*.gz
+    )
 fi
 
 date +'End: %F %T %s.%N'
