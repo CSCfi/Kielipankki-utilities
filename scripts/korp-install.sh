ade459099b994d693b87239ab09418c51026af92
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2016-01-25 13:01:45 +0200

    korp-install.sh: Fixed installing Korp news
    scripts/korp-install.sh:
      install_news(): Fixed the news source directory to be in the news
        component instead of the frontend. Previously, the news did not get
        updated.

--- scripts/korp-install.sh
+++ scripts/korp-install.sh
@@ -294,7 +294,7 @@ install_news () {
     git_get news master master
     commit_sha1_full_news=$(git rev-parse HEAD)
     mkdir -p $root_frontend/$target/news/json
-    run_rsync $local_git_prefix$comp/json/ $root_frontend/$target/news/json/ \
+    run_rsync ${local_git_prefix}news/json/ $root_frontend/$target/news/json/ \
 	--filter 'protect /*' \
 	--exclude '.git*'
     log INFO "Installed: news to $targetdir/news from master ($commit_sha1_full_news)"
