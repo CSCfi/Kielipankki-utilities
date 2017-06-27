cdf87ad1cfc6dbc28a94e1e0a03345ec929c8799
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2017-06-27 08:50:51 +0300

    corp/byu: Added byu-convert-sbatch.sh
    Added corp/byu/byu-convert-sbatch.sh:
      A Bash script for submitting SLURM batch job to convert BYU corpora from
      the word/lemma/PoS format to VRT.

---
+++ corp/byu/byu-convert-sbatch.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+progname=$(basename $0)
+progdir=$(dirname $0)
+
+scriptdir=$progdir/../../scripts
+
+usage_header="Usage: $progname [options] wlp_input.txt ...
+
+Submit a SLURM batch job to convert BYU corpora from WLP to VRT."
+
+action=sbatch
+
+optspecs='
+n|dry-run { action=cat }
+l|log-dir=DIR "."
+timelimit=MINS "10"
+memory=MB "1000"
+metadata-file=FILE
+v|verbose
+'
+
+. $scriptdir/korp-lib.sh
+
+# Process options
+eval "$optinfo_opt_handler"
+
+
+if [ $action = sbatch ] && ! find_prog sbatch > /dev/null; then
+    error "Please run in a system with SLURM installed."
+fi
+
+
+for file in "$@"; do
+    jobname_base="$(basename $file .txt)"
+    jobname="byu_$jobname_base"
+    if [ -e "$file.vrt" ]; then
+	echo "Skipping $file: $file.vrt already exists"
+	continue
+    fi
+
+    if [ "x$verbose" != x ]; then
+	cat <<EOF
+Submitting job "$jobname" to partition "serial"
+Max run time: $timelimit mins
+RAM per CPU: $memory MiB
+EOF
+fi
+
+    $action <<EOF
+#! /bin/bash -l
+#SBATCH -J $jobname
+#SBATCH -o $log_dir/byu_log-$jobname_base-%j.out
+#SBATCH -e $log_dir/byu_log-$jobname_base-%j.err
+#SBATCH -t $timelimit
+#SBATCH --mem-per-cpu $memory
+#SBATCH -n 1
+#SBATCH -p serial
+
+. $scriptdir/korp-lib.sh
+
+echo Job: \$SLURM_JOB_ID \$SLURM_JOB_NAME
+echo Input: "$file"
+$progdir/byu-convert.sh --metadata-file "$metadata_file" --verbose \
+    "$file" > "$file.vrt"
+EOF
+
+done
