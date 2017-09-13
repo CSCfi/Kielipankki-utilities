1777f306c65c54580d6372b8d87b3018b544097d
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2017-09-13 11:17:39 +0300

    korp-lib.sh: cleanup: Kill descendant processes
    scripts/korp-lib.sh:
      At cleanup, kill strictly the descendants of the current process, instead
      of killing all processes in the same process group, to avoid killing
      other processes in the same pipeline and to kill processes that have
      changed their group. This is implemented in function kill_descendants.

--- scripts/korp-lib.sh
+++ scripts/korp-lib.sh
@@ -340,6 +340,41 @@ time_cmd () {
     fi
 }
 
+# kill_descendants [--and-self] [-SIG] pid [...]
+#
+# Kill all the descendant processes of the process(es) with the pid(s)
+# listed. If --and-self is specified, also kill the processes
+# themselves. If -SIG is specified, it is passed to kill.
+#
+# Adapted from https://stackoverflow.com/a/26966800
+kill_descendants () {
+    local self sig pids pid children
+    self=
+    sig=
+    if [ "x$1" = "x--and-self" ]; then
+	self=1
+	shift
+    fi
+    if [ "${1#-}" != "$1" ]; then
+	sig=$1
+	shift
+    fi
+    pids="$@"
+    for pid in $pids; do
+	children=$(pgrep -P $pid)
+	if [ "x$children" != x ]; then
+            kill_descendants --and-self $sig $children
+        fi
+    done
+    if [ "x$self" != x ]; then
+	kill $sig $pids
+	# According to https://stackoverflow.com/a/5722874, this
+	# should prevent the "Terminated" messages from background
+	# processes, but it does not seem to. What would work?
+	wait $pids 2> /dev/null
+    fi
+}
+
 cleanup () {
     if [ "x$tmp_prefix" != "x" ] && [ "x$cleanup_on_exit" != x ]; then
 	rm -rf $tmp_prefix.*
@@ -347,14 +382,10 @@ cleanup () {
     # Register a no-op handler for SIGTERM, so that kill does not
     # trigger it recursively
     trap ':' TERM
-    # Kill all processes in the process group of the running script.
-    # This does not kill processes that have changed their process
-    # group (cf.
-    # http://stackoverflow.com/questions/360201/kill-background-process-when-shell-script-exit).
-    # FIXME: If the script is a part of a pipe, this also kills all
-    # the other processes in the pipe. It would probably be better to
-    # kill recursively all the descendants of the current process.
-    kill -- -$$ 2> /dev/null
+    # Kill all the descendant processes of the running script. If the
+    # script is a part of a pipe, this avoids killing the other
+    # processes in the pipe, as killing by process group would do.
+    kill_descendants $$ 2> /dev/null
 }
 
 cleanup_abort () {
