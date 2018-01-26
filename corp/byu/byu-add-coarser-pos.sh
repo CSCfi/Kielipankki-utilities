0717c0903e390aeaef0abf65fb4eb8c802bfa863
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2018-01-26 15:54:52 +0200

    corp/byu: Scripts for adding a coarser PoS attr
    Added scripts and a mapping for adding a coarser part-of-speech attribute
    and a morphological features attribute to the BYU corpora.
    - corp/byu/byu-make-coarser-pos.py: Generate the values for a coarser PoS
      tag and morphological features based on PoS tags in the input.
    - corp/byu/byu-ud2-posmap.tsv: A mapping from BYU (CLAWS7) PoS tags to UD2
      PoS tags, to used by byu-make-coarser-pos.py.
    - corp/byu/byu-add-coarser-pos.sh: A wrapper script using
      scripts/cwbdata-add-attrs.sh and byu-make-coarser-pos.py to add the new
      attributes to CWB corpus data.

---
+++ corp/byu/byu-add-coarser-pos.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+
+# Add a coarser part of speech to the BYU corpora.
+#
+# Usage: byu-add-coarser-pos.sh corpus ...
+
+
+progdir=$(dirname $0)
+progname=$(basename $0)
+
+scriptdir=$progdir/../../scripts
+
+. $scriptdir/korp-lib.sh
+
+
+make_coarser_pos=$progdir/byu-make-coarser-pos.py
+cwbdata_add_attrs=$scriptdir/cwbdata-add-attrs.sh
+
+mapping_file_src=$progdir/byu-ud2-posmap.tsv
+mapping_file=$tmp_prefix.map.tsv
+cut -f1,3,4 $mapping_file_src > $mapping_file
+
+corpora=$(list_corpora "$@")
+
+
+add_coarser_pos () {
+    local corp
+    corp=$1
+    $cwbdata_add_attrs --input-attrs "pos" --new-attrs "pos_major/ msd_ambig" \
+	--generator "$make_coarser_pos $mapping_file" --verbose $corp
+}
+
+
+for corp in $corpora; do
+    add_coarser_pos $corp
+done
---
+++ corp/byu/byu-make-coarser-pos.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Generate attributes for coarser part-of-speech tags and morphological features
+"""
+
+
+import sys
+import argparse
+from collections import OrderedDict
+from xml.sax.saxutils import escape
+
+from io import open
+
+
+class CoarserPosGenerator:
+
+    def __init__(self, args):
+        self._pos_map = self._read_pos_map_file(args.pos_map_file)
+        self._filenames = args.filenames or []
+        self._opts = args
+
+    def _read_pos_map_file(self, fname):
+
+        def xml_escape(val):
+            return escape(val, {'"': '&quot;'})
+
+        pos_map = {}
+        with open(fname, 'r', encoding='utf8', errors='replace') as f:
+            for line in f:
+                fields = [field.strip().split('/')[0]
+                          for field in line.strip().split('\t')]
+                for i in [1, 2]:
+                    fields[i] = xml_escape(fields[i])
+                pos_map[fields[0]] = (fields[1], fields[2])
+                pos_map[xml_escape(fields[0])] = (fields[1], fields[2])
+        return pos_map
+                
+    def process(self):
+        if self._filenames:
+            for filename in self._filenames:
+                with open(filename, 'r', encoding='utf8',
+                          errors='replace') as f:
+                    self._process_stream(filename, f)
+        else:
+            self._process_stream('(stdin)', sys.stdin)
+
+    def _process_stream(self, filename, f):
+        # sys.stdout.write(repr(self._pos_map))
+
+        def make_featset_value(lst):
+            # If all the values of the feature set are the same,
+            # output the value only once.
+            if len(lst) > 1:
+                if all(lst[i] == lst[0] for i in range(1, len(lst))):
+                    lst = [lst[0]]
+            return '|' + '|'.join(lst) + '|'
+
+        for linenr, line in enumerate(f):
+            byu_poses = [field.strip()
+                         for field in line.strip().strip('|').split('|')]
+            # Python does not have built-in ordered set, so use
+            # OrderedDict with dummy values.
+            mapped_poses = OrderedDict()
+            for byu_pos in byu_poses:
+                # sys.stdout.write(byu_pos + '\n')
+                # sys.stdout.flush()
+                if byu_pos not in self._pos_map:
+                    sys.stderr.write(
+                        'Warning: Tag {byu_pos} on line {linenr} not found in'
+                        ' the mapping; replacing with X\n'
+                        .format(byu_pos=byu_pos, linenr=linenr + 1))
+                    mapped_poses[('X', '_')] = ''
+                else:
+                    mapped_poses[self._pos_map[byu_pos]] = ''
+            sys.stdout.write(
+                '\t'.join(make_featset_value(
+                    [pos_morph[idx] for pos_morph in mapped_poses.keys()])
+                          for idx in range(2))
+                + '\n')
+
+
+def getargs():
+    argparser = argparse.ArgumentParser(
+        description='''Generate attributes for coarser part-of-speech tags and morphological features''')
+    argparser.add_argument(
+        'pos_map_file',
+        help=('File with lines containing the (converted) BYU PoS tag, the'
+              ' corresponding coarser PoS tag and morphological features,'
+              ' separated tabs'))
+    argparser.add_argument(
+        'filenames', nargs='*',
+        help=('Input files containing the cleaned-up BYU PoS attribute with'
+              ' feature set values, typically extracted from the CWB-data with'
+              ' cwb-decode'))
+    return argparser.parse_args()
+
+
+def main():
+    CoarserPosGenerator(getargs()).process()
+
+
+if __name__ == '__main__':
+    main()
