49b70961dcbab10f37864c687e2ad14c7cfa00b6
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2018-08-29 13:26:44 +0300

    prevrt/scriptutil.py: Changed argspecs format
    scripts/prevrt/scriptutil.py:
      get_argparser, argparser_add_args: Changed the format for specifying
      command-line arguments for ArgumentParser:
      – Lists or tuples of 2 or 3 items: (argnames, help, [argdict]).
      – argnames may specify all the option names in a single string, separated
        by spaces or vertical bars.
      – argnames may specify metavar, nargs and dest, reducting the need to use
        argdict.
      – If argdict contains a default, append information on it to the usage
        string.
      Show standard options after script-specific ones (get_argparser).
      Upper-case metavar names in standard options (get_argparser).

--- scripts/prevrt/scriptutil.py
+++ scripts/prevrt/scriptutil.py
@@ -14,6 +14,7 @@ directly or modified.
 
 
 import os
+import re
 import sys
 import traceback
 
@@ -47,44 +48,108 @@ def get_argparser(argspecs=None, version='undefined', **kwargs):
     --out (-o), --in-place (-i), --backup (-b) and --version, and
     other arguments specified in `argspecs`.
     Arguments:
-      `argspecs`: a list of argument specifications, which are pairs
-          (argnames, argdict) where argnames is either a single string
-          or a list of strings representing argument names and argdict
-          is a dictionary of the keyword arguments to be passed to
-          `ArgumentParser.add_argument`.
+      `argspecs`: a list of command-line argument specifications; see
+          `argparser_add_args` for the format
       `version`: the version string shown by --version.
       `**kwargs` is passed directly to `ArgumentParser`.
     """
     argparser = ArgumentParser(**kwargs)
     common_argspecs = [
-        ('infile',
-         dict(nargs='?', metavar='file',
-              help='input file (default stdin)')),
-        (['--out', '-o'],
-         dict(dest='outfile', metavar='file',
-              help='output file (default stdout)')),
-        (['--in-place', '-i'],
-         dict(dest='inplace', action='store_true',
-              help='overwrite input file with output')),
-        (['--backup', '-b'],
-         dict(metavar='bak',
-              help='keep input file with suffix bak')),
-        (['--version'],
-         dict(action='store_true',
-              help='print {} and exit'.format(version))),
+        [
+            # Before arguments in argspecs
+            ('infile = FILE /?',
+             'input FILE (default stdin)'),
+        ],
+        [
+            # After arguments in argspecs
+            ('--out -o = FILE -> outfile',
+             'output FILE (default stdout)'),
+            ('--in-place -i -> inplace',
+             'overwrite input file with output'),
+            ('--backup -b = BAK',
+             'keep input file with suffix BAK'),
+            ('--version',
+             'print {} and exit'.format(version)),
+        ],
     ]
-    argparser_add_args(argparser, common_argspecs)
-    argparser_add_args(argparser, argspecs)
+    argparser_add_args(
+        argparser, common_argspecs[0] + (argspecs or []) + common_argspecs[1])
     return argparser
 
 
 def argparser_add_args(argparser, argspecs):
-    """Add the arguments specified in argspecs to ArgumentParser argparser."""
+    """Add the arguments specified in argspecs to ArgumentParser argparser.
+
+    `argspecs` is a list of command-line argument specifications,
+    which are lists or tuples of 2 or 3 elements: (argnames, help,
+    [argdict]) where argnames is either a single string or a list or
+    tuple of strings representing argument names (and optionally some
+    options, see below), help is the usage string and argdict is a
+    dictionary of the keyword arguments to be passed to
+    `ArgumentParser.add_argument`. If argdict contains a default
+    value, information about it is appended to the usage string,
+    unless the usage string already contains the word "default".
+
+    argnames (or its first element) is of the following form:
+      argname (("|"|" ") argname)* ("=" metavar)? ("/" nargs)? ("->" dest)?.
+    Each argname is an alternative name for the argument, and metavar,
+    nargs and dest correspond to the keyword arguments of
+    `ArgumentParser.add_argument` with the same names. The components
+    may have spaces between them. If no metavar is specified and
+    argdict has no action, add action='store_true'.
+    """
     if argspecs:
-        for argnames, argdict in argspecs:
-            if not isinstance(argnames, list):
-                argnames = [argnames]
-            argparser.add_argument(*argnames, **(argdict or {}))
+        for argspec in argspecs:
+            _argparser_add_arg(argparser, argspec)
+
+
+def _argparser_add_arg(argparser, argspec):
+    """Add the argument specified in argspec to ArgumentParser argparser.
+
+    See `argparser_add_args` for the format of `argspec`.
+    """
+    if len(argspec) < 2:
+        raise BadCode(
+            'Argument specification needs at least name and help text')
+    argnames, arghelp = argspec[:2]
+    # Convert argnames to a list for futher processing
+    if isinstance(argnames, str):
+        argnames = [argnames]
+    elif isinstance(argnames, tuple):
+        argnames = list(argnames)
+    argdict = (argspec[2] or {}) if len(argspec) > 2 else {}
+    # Add information on the possible default value to the usage
+    # message, unless it already contains the string "default".
+    if 'default' in argdict and 'default' not in arghelp:
+        arghelp += ' (default: %(default)s)'
+    argdict['help'] = arghelp
+    # Parse (the first element of) argnames
+    mo = re.match(r"""(?P<names> [^=/>]+?)
+                      (?: \s* = \s* (?P<metavar> \S+?)) ?
+                      (?: \s* / \s* (?P<nargs> \S+?)) ?
+                      (?: \s* -> \s* (?P<dest> \S+?)) ? $ """,
+                  argnames[0], re.VERBOSE)
+    if not mo:
+        raise BadCode('Invalid argument specification string')
+    argdict0 = dict((key, val) for key, val in mo.groupdict().items()
+                    if val is not None)
+    argnames[0:1] = re.split(r'\s*(?:\s+|\|)\s*', argdict0['names'].strip())
+    del argdict0['names']
+    argdict0.update(argdict)
+    argdict = argdict0
+    if 'metavar' not in argdict and 'action' not in argdict:
+        argdict['action'] = 'store_true'
+    # Split argument name by spaces and/or vertical bars
+    argnames[0:1] = re.split(r'\s*(?:\s+|\|)\s*', argnames[0].strip())
+    # If alternative option argument names omit the leading hyphens,
+    # add them.
+    isoptarg = (argnames[0][0] == '-')
+    if isoptarg:
+        for i in range(1, len(argnames)):
+            if argnames[i][0] != '-':
+                prefix = ('-' if len(argnames[i]) == 1 else '--')
+                argnames[i] = prefix + argnames[i]
+    argparser.add_argument(*argnames, **argdict)
 
 
 def error_exit(*msg):
