dcacd92d1f80e5c6e11517597dad50b941dc3d7e
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2018-12-04 21:44:23 +0200

    New tool to REDACT long "words"
    Mainly to prevent failures to annotate at all.
    
    vrt-redact-long
    vrtargslib.py (for version number)

--- scripts/prevrt/vrtargslib.py
+++ scripts/prevrt/vrtargslib.py
@@ -5,7 +5,7 @@ from string import ascii_letters, digits as ascii_digits
 from tempfile import mkstemp
 import os, sys, traceback
 
-VERSION = '0.6.2 (2018-12-02)'
+VERSION = '0.6.3 (2018-12-04)'
 
 class BadData(Exception): pass # stack trace is just noise
 class BadCode(Exception): pass # this cannot happen
