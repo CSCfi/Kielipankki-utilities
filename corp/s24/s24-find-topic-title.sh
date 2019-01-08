b5379497b31db8e7804c242264d7a96a0d1ac1a9
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2019-01-08 14:15:00 +0200

    s24-find-topic-title.sh: Try 10 threads
    corp/s24/s24-find-topic-title.sh:
      Try to get the topic titles from at most ten threads if the first does
      not succeed.

--- corp/s24/s24-find-topic-title.sh
+++ corp/s24/s24-find-topic-title.sh
@@ -23,44 +23,61 @@ fi
 topic_num=$1
 shift
 
-thread_topic_level=$(
-    perl -ne '
-	if (/^<text.*topics="((?:[0-9]+,)*'$topic_num'(?:,[0-9]+)*)"/) {
-	    @topics = split (",", $1);
-	    for ($level = 0; $level <= $#topics; $level++) {
-		last if ($topics[$level] == '$topic_num');
+topic_title=DUMMY
+try_threads=10
+
+round=1
+
+topic_title=
+while [ $round -le $try_threads ] && [ "x$topic_title" = x ]; do
+
+    thread_topic_level=$(
+	perl -ne '
+	    if (/^<text.*topics="((?:[0-9]+,)*'$topic_num'(?:,[0-9]+)*)"/) {
+	        $count++;
+		if ($count < '$round') {
+		    next;
+		}
+		@topics = split (",", $1);
+		for ($level = 0; $level <= $#topics; $level++) {
+		    last if ($topics[$level] == '$topic_num');
+		}
+		# The topic numbers are listed from bottom up, but return
+		# level number so that the topmost level is 1.
+		$level = $#topics - $level + 1;
+		($thread) = /thread="(.*?)"/;
+		print "$thread $level\n";
+		exit;
 	    }
-	    # The topic numbers are listed from bottom up, but return
-	    # level number so that the topmost level is 1.
-	    $level = $#topics - $level + 1;
-	    ($thread) = /thread="(.*?)"/;
-	    print "$thread $level\n";
-	    exit;
-	}
-    ' "$@"
-)
+	' "$@"
+    )
 
-if [ "x$thread_topic_level" = x ]; then
-    echo "Threads with topic $topic_num not found" > /dev/stderr
-    exit 1
-fi
+    if [ "x$thread_topic_level" = x ]; then
+	echo "Threads with topic $topic_num not found" > /dev/stderr
+	exit 1
+    fi
 
-thread=${thread_topic_level% *}
-topic_level=${thread_topic_level#* }
+    thread=${thread_topic_level% *}
+    topic_level=${thread_topic_level#* }
 
-logfile=$TMPDIR/$$.wget.log
+    logfile=$TMPDIR/$$.wget.log
 
-topic_title=$(
-    wget -q -O - -o $logfile 'https://keskustelu.suomi24.fi/t/'$thread'/' |
-	grep '<a property="item" typeof="WebPage"' |
-	grep -v '>Keskustelu24<' |
-	head -n$topic_level |
-	tail -1 |
-	sed -e 's/.*<span property="name">//; s/<\/span>.*//;'
-)
+    topic_title=$(
+	wget -O - -o $logfile 'https://keskustelu.suomi24.fi/t/'$thread'/' |
+	    grep '<a property="item" typeof="WebPage"' |
+	    grep -v '>Keskustelu24<' |
+	    head -n$topic_level |
+	    tail -1 |
+	    sed -e 's/.*<span property="name">//; s/<\/span>.*//;'
+    )
+    # echo $round $thread $topic_level $topic_title
+    round=$(($round + 1))
+done
 
 if [ "x$topic_title" = x ]; then
-    echo "Error retrieving thread $thread for topic $topic_num:" > /dev/stderr
+    echo "Error retrieving threads topic $topic_num (tried $try_threads threads):" \
+	 > /dev/stderr
+    echo "Wget log output for the last thread $thread:" > /dev/stderr
     cat $logfile > /dev/stderr
     exit 1
 fi
