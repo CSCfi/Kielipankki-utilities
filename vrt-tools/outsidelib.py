ace0c28b0718fec90191b17150541aa83b141a8f
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-02-19 10:01:08 +0200

    Draft hrt-tokenizer from finnish-tagtools
    Also a minor fix in tr-based tokenizer - some udpipe code had crept in
    
    hrt-tokenize-finnish-hfst
    hrt-tokenize-tr
    
    outsidelib.py

--- vrt-tools/outsidelib.py
+++ vrt-tools/outsidelib.py
@@ -7,3 +7,9 @@ UDPIPE = '/appl/ling/udpipe/1.2.0/bin/udpipe'
 UDPIPEMODEL = (
     '/appl/ling/udpipe/models/udpipe-ud-2.3-181115/{}-2.3-181115.udpipe'
 )
+
+HFSTTOKENIZE = '/appl/ling/hfst/3.15.0/bin/hfst-tokenize'
+OMORFITOKENIZE = (
+    '/appl/ling/finnish-tagtools/1.3.2/share/finnish-tagtools'
+    '/omorfi_tokenize.pmatch'
+)
