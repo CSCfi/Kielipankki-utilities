664a34b35d7a8265590bb0b13146eba1f3da2378
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-02-19 08:15:03 +0200

    Draft tools to add udpipe as parser
    hrt-udpipe : tokenize and parse
    vrt-udpipe : parse
    
    hrt-tokenize-udpipe : import udpipe from new outsidelib
    vrt-tdp-alpha-marmot : import marmot from new outsidelib
    
    outsidelib.py : pathnames of external tools and models

---
+++ vrt-tools/outsidelib.py
@@ -0,0 +1,9 @@
+MARMOTJAR = '/proj/kieli/varpunen/marmot-2014-10-22.jar'
+
+MARMOTMODEL = '/proj/kieli/varpunen/models/fin_model.marmot'
+
+UDPIPE = '/appl/ling/udpipe/1.2.0/bin/udpipe'
+
+UDPIPEMODEL = (
+    'appl/ling/udpipe/models/udpipe-ud-2.3-181115/{}-2.3-181115.udpipe'
+)
