d155b80df60b4aa24fc25c8a12593c2d40d0ecb1
eaxelson <erik.axelson@helsinki.fi>
2019-04-20 14:51:01 +0300

    Remove spaces from titles.

--- corp/semfinlex/asd/mark-heading-paragraphs.pl
+++ corp/semfinlex/asd/mark-heading-paragraphs.pl
@@ -23,7 +23,7 @@ while (<>) {
     elsif (/^<\/saa:(Osa|Luku|Pykala)TunnusKooste/) { $tunnuskooste = 0; $waiting = 1; }
     elsif (/^<\/saa:SaadosOtsikkoKooste/) { $saadosotsikkokooste = 0; }
     elsif ($tunnuskooste eq 1 || $saadosotsikkokooste eq 1) { $title .= $_; $title =~ s/\n/ /g; }
-    elsif ($waiting eq 1) { $waiting = 0; $delayed .= join('','<</paragraph>>',"\n"); $title =~ s/^ +//; $title =~ s/ +$//; $title =~ s/"/&quot;/g ;$title =~ s/'/&apos;/g; $title =~ s/<[^>]*>//g; unless ($title eq "") { $delayed = join('','<<title="',$title,'">>',"\n",$delayed); } $title = ""; }
+    elsif ($waiting eq 1) { $waiting = 0; $delayed .= join('','<</paragraph>>',"\n"); $title =~ s/<[^>]*>//g; $title =~ s/"/&quot;/g ;$title =~ s/'/&apos;/g; $title =~ s/^ +//; $title =~ s/ +$//; $title =~ s/ +/ /g; unless ($title eq "") { $delayed = join('','<<title="',$title,'">>',"\n",$delayed); } $title = ""; }
 
     if ($tunnuskooste eq 1 || $saadosotsikkokooste eq 1 || $waiting eq 1) { $delayed .= $_; }
     else
