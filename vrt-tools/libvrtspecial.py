5b9839c0bdcd6a9250b99662b85d44ba8b860dae
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-05-08 17:22:10 +0300

    Break vertical bar in attribute value
    Instead of replacing with slash, replace with broken vertical bar
    
    libvrtspecial.py

--- vrt-tools/libvrtspecial.py
+++ vrt-tools/libvrtspecial.py
@@ -8,6 +8,8 @@ import html, sys
 # At least one contains vertical bar (not a main department):
 # 'X3M | De bästa intervjuerna'
 # Eventually decided to replace that with /.
+# Have reconsidered and now replace with ¦ instead.
+# Incidentally, any replacement breaks an URL or such.
 #
 # No " ' < > seen so far.
 
@@ -16,7 +18,7 @@ def finish_av(value):
     if '"' in value: print('attr', repr(value), file = sys.stderr)
     if '|' in value:
         print('attr in ', repr(value), file = sys.stderr)
-        value = value.replace('|', '/')
+        value = value.replace('|', '\N{BROKEN VERTICAL BAR}')
         print('attr out', repr(value), file = sys.stderr)
     return html.escape(value, quote = True)
 
@@ -30,5 +32,5 @@ def finish_t(token):
     # if '<' in token: print(repr(token), file = sys.stderr)
     # if '>' in token: print(repr(token), file = sys.stderr)
     # if '&' in token: print(repr(token), file = sys.stderr)
-    if '|' in token: print(repr(token), file = sys.stderr)
+    if '|' in token: print('token', repr(token), file = sys.stderr)
     return html.escape(token.replace(' ', '\xa0'), quote = False)
