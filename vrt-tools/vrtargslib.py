d3bfc9558309dec6ca4a90c9a2527b6aee0d2c0e
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-09-13 14:51:35 +0300

    Point vrt-sparv-* to publicly installed tools in Taito
    Version to 0.8.5
    
    outsidelib.py

--- vrt-tools/vrtargslib.py
+++ vrt-tools/vrtargslib.py
@@ -5,7 +5,7 @@ from string import ascii_letters, digits as ascii_digits
 from tempfile import mkstemp
 import os, sys, traceback
 
-VERSION = '0.8.4 (2019-09-13)'
+VERSION = '0.8.5 (2019-09-13)'
 
 class BadData(Exception): pass # stack trace is just noise
 class BadCode(Exception): pass # this cannot happen
