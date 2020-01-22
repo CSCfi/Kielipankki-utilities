9ecfccf431482afc269cf99e5dca6fb1df2acda2
eaxelson <erik.axelson@helsinki.fi>
2020-01-22 13:29:18 +0200

    Warn about source txt files that do not exist.

--- corp/ethesis/retokenize_en/split-files.sh
+++ corp/ethesis/retokenize_en/split-files.sh
@@ -18,7 +18,16 @@ do
 	do
 	    # change filename info ("FILENAME_", newpar, sent_id) into "# FILENAME: ..."
 	    # todo: renumber the sentences
-	    cat $file | perl -pe 's/\n/¤/g;' | perl -pe 's/# newpar¤# sent_id = [^¤]+¤# text = "FILENAME_([^"]+)"/# FILENAME: \1¤/g;' | perl -pe 's/¤/\n/g;' | ./split-conllu-files.pl;
+	    cat $file | perl -pe 's/\n/¤/g;' | perl -pe 's/# newpar¤# sent_id = [^¤]+¤# text = "FILENAME_([^"]+)"/# FILENAME: \1¤/g;' | perl -pe 's/¤/\n/g;' > tmp;
+	    filenames=`cat tmp | egrep '^# FILENAME' | perl -pe 's/# FILENAME\: //;'`;
+	    for filename in $filenames;
+	    do
+		if !(ls $filename > /dev/null 2> /dev/null); then
+		    echo "Warning: no such source file: "$dir"/"$subdir"/"$filename;
+		fi
+	    done
+	    cat tmp | ./split-conllu-files.pl;
+	    rm tmp;
 	done
 	rm split-conllu-files.pl;
 	cd ..;
