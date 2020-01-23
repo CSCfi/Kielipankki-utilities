a628503b26e834736c29359720a5fbdcbb039325
eaxelson <erik.axelson@helsinki.fi>
2020-01-23 15:25:22 +0200

    Remove option --test.

--- corp/ethesis/retokenize_en/combine-files.sh
+++ corp/ethesis/retokenize_en/combine-files.sh
@@ -43,13 +43,7 @@ do
 	    #   Unicode line and paragraph separators (U+2028, U+2029) and soft hyphens (U+00AD) and some other strange characters
 	    # - convert FIGURE SPACE (U+2007) and NARROW NO-BREAK SPACE (U+202F) (and also THIN SPACE, U+2009) to NBSPs
 	    # - convert other spaces into oridinary spaces
-	    if [ "$1" = "--test" -o "$2" = "--test" ]; then
-		head -10 $file > tmp;
-	    else
-		cat $file > tmp;
-	    fi
-	    cat tmp | perl -C -pe 's/[\x{0000}-\x{0008}\x{000B}\x{000C}\x{000E}-\x{001F}\x{00AD}\x{007F}-\x{009F}\x{2028}\x{2029}\x{00AD}\x{2028}\x{FEFF}\x{FFFF}]//g; s/[\x{2007}\x{202F}\x{2009}]/\x{00A0}/g; s/[\x{0085}\x{1680}\x{2000}-\x{200A}\x{202F}\x{205F}\x{3000}]/ /g;' >> "ALL"$nall.TXT;
-	    rm tmp;
+	    cat $file | perl -C -pe 's/[\x{0000}-\x{0008}\x{000B}\x{000C}\x{000E}-\x{001F}\x{00AD}\x{007F}-\x{009F}\x{2028}\x{2029}\x{00AD}\x{2028}\x{FEFF}\x{FFFF}]//g; s/[\x{2007}\x{202F}\x{2009}]/\x{00A0}/g; s/[\x{0085}\x{1680}\x{2000}-\x{200A}\x{202F}\x{205F}\x{3000}]/ /g;' >> "ALL"$nall.TXT;
 	    echo "" >> "ALL"$nall.TXT;
 	done
 	cd ..;
