1139aed3c32e619713404cf3b2e17303e523a597
eaxelson <erik.axelson@helsinki.fi>
2020-02-10 19:14:13 +0200

    Add text attribute 'type', if missing. Add text attribute 'faculty_directory'. Harmonize attribute names, add missing attributes with an empty value.

--- corp/ethesis/retokenize_en/conllu-to-vrt.sh
+++ corp/ethesis/retokenize_en/conllu-to-vrt.sh
@@ -15,9 +15,19 @@ do
     fi
 done
 
+# text attributes "type" and "faculty_directory"
+text_type=""; # add if missing
+text_faculty_directory=""; # add to all texts
+
 cd ethesis_en;
 for dir in gradut vaitokset;
 do
+    if [ "$dir" = "gradut" ]; then
+	text_type="Master&apos;s thesis";
+    else
+	text_type="Doctoral dissertation";
+    fi
+
     cd $dir;
     subdirs="bio_ja_ymparistot elainlaaketiede farmasia humanistinen kayttaytymistiede laaketiede maajametsatiede matemaattis oikeustiede teologinen valtiotiede";
     if [ "$dir" = "gradut" ]; then
@@ -25,6 +35,33 @@ do
     fi
     for subdir in $subdirs;
     do
+	case "$subdir" in
+	    "aleksanteri-instituutti") text_faculty_directory="Aleksanteri Institute"
+	    ;;
+	    "bio_ja_ymparistot") text_faculty_directory="Faculty of Biological and Environmental Sciences"
+	    ;;
+	    "elainlaaketiede") text_faculty_directory="Faculty of Veterinary Medicine"
+	    ;;
+	    "farmasia") text_faculty_directory="Faculty of Pharmacy"
+	    ;;
+	    "humanistinen") text_faculty_directory="Faculty of Arts"
+	    ;;
+	    "kayttaytymistiede") text_faculty_directory="Faculty of Behavioural Sciences"
+	    ;;
+	    "laaketiede") text_faculty_directory="Faculty of Medicine"
+	    ;;
+	    "maajametsatiede") text_faculty_directory="Faculty of Agriculture and Forestry"
+	    ;;
+	    "matemaattis") text_faculty_directory="Faculty of Science"
+	    ;;
+	    "oikeustiede") text_faculty_directory="Faculty of Law"
+	    ;;
+	    "teologinen") text_faculty_directory="Faculty of Theology"
+	    ;;
+	    "valtiotiede") text_faculty_directory="Faculty of Social Sciences"
+	    ;;
+	esac
+
 	cd $subdir;
 	if [ "$verbose" = "true" ]; then
 	    echo "Generating VRT files in directory "$dir"/"$subdir;
@@ -42,6 +79,16 @@ do
 	    $vrttooldir/vrt-keep -i -n 'word,id,lemma,upos,xpos,feats,head,deprel,deps,misc' $vrtfile;
 	    $vrttooldir/vrt-rename -i -m id=ref -m head=dephead -m feats=msd -m upos=pos $vrtfile;
 	    # cat file | ./msd-bar-to-space.pl > tmp && mv tmp $vrtfile;
+	    #
+	    # harmonize attribute names, add missing attributes with an empty value, represent empty values as "_":
+	    #
+	    perl -pe 'if (/^<text[ >]/) { s/ citation_(title|date|keywords=")/ \1/g; s/citation_abstract_html_url="/url="/; s/=""/="_"/g; }';
+	    perl -pe 'if (/^<text[ >]/) { foreach my $attr ("citation_language","citation_authors","url","keywords","citation_pdf_url","date","title","faculty","subject","type","datefrom","dateto") { unless (/${attr}="/) { s/>/ ${attr}="_">/; } } }';
+	    # - add datefrom and dateto (these are missing from 40 files that don't have a metadata file)
+	    # - add timefrom="000000" timeto="235959" (no file has these, but korp requires them?)
+	    #
+	    # Add type, if empty. Define faculty directory.
+	    perl -pe 'if (/^<text /) { s/>/ faculty_directory="'$text_faculty_directory'">/; s/type="_"/type="'$text_type'"/; }'
 
 	done
 	cd ..;
