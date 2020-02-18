28fe3f75a9c7e6e052be9ab126aa35a457504faa
Sam Hardwick <sam.hardwick@iki.fi>
2020-02-18 11:38:55 +0200

    Add example with custom api requests

---
+++ python-api-proposal/these-requests-should-be-fast.py
@@ -0,0 +1,8 @@
+# Giving arbitrary Python functions to run over the entire database is
+# inevitably slow. These requests should be possible to make fast
+
+from kielipankki.corpora import suomi24
+
+results = suomi24.get_texts_by_any_word([word1, word2, word3])
+results = suomi24.get_sentences_by_NER('<NERTag>')
+results = suomi24.get_texts_by_time(time_interval)
