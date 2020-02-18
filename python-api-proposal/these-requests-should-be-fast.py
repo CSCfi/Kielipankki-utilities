ec4e7d2e25cf5a1aa3b8cf46884e8a6aa1a1d76d
Sam Hardwick <sam.hardwick@iki.fi>
2020-02-18 11:43:11 +0200

    Add possibility of chaining requests to get logical conjunction

--- python-api-proposal/these-requests-should-be-fast.py
+++ python-api-proposal/these-requests-should-be-fast.py
@@ -6,3 +6,5 @@ from kielipankki.corpora import suomi24
 results = suomi24.get_texts_by_any_word([word1, word2, word3])
 results = suomi24.get_sentences_by_NER('<NERTag>')
 results = suomi24.get_texts_by_time(time_interval)
+
+results = suomi24.get_sentences_by_NER('<NERTag>').get_texts_by_time(time_interval)
