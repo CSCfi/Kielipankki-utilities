CSCfi/Kielipankki-utilities

commit 1ded0fa81b60d2845e8a92306290b1958608869a

cwbdata2vrt-simple: Undef annotation val as empty
scripts/cwbdata2vrt-simple.sh:
- Output undefined annotation values of structural attributes as empty
  strings. (Such undefined values probably should not occur but in some
  cases they do.)

scripts/cwbdata2vrt-simple.sh
@@ -165,12 +165,15 @@ process_tags_single () {
 }
 
 process_tags_multi () {
+    local corp
+    corp=$1
     perl -ne '
         BEGIN {
             $prevtag = $tag = $attrs = "";
+	    $cpos = 0;
         }
-        if (/^(<[^\/_\s]*)(?:_([^ ]*) (.*))?>$/) {
-	    # Structure start tag with annotation value
+        if (/^(<[^\/_\s]*)(?:_([^ ]*)( )?(.*))?>$/) {
+	    # Structure start tag, possibly with an annotation value
             $tag = $1;
             if ($tag ne $prevtag && $attrs) {
                 print "$prevtag$attrs>\n";
@@ -179,7 +182,21 @@ process_tags_multi () {
             $prevtag = $tag;
 	    if ($2) {
 		$attrname = $2;
-		$attrval = $3;
+		# If the annotation is defined but has no value, not
+		# even an empty string (a line of the form
+		# <struct_attr>), it is treated as an empty string.
+		# Alternatively, we could have special value for such
+		# undefined values, but they probably should not occur
+		# anyway. One option might be to add (optionally) a
+		# special VRT comment indicating the issue.
+		if (! $3) {
+		    $struct = substr($prevtag, 1);
+		    print STDERR ("'"$progname"': Warning: corpus '"$corp"',"
+		                  . " position $cpos: structure \"$struct\":"
+				  . " undefined value for attribute"
+				  . " \"$attrname\" treated as empty string\n");
+		}
+		$attrval = $4;
 		'"$perl_encode_entities_attrval"'
 		$attrs .= " $attrname=\"$attrval\"";
 	    }
@@ -199,6 +216,7 @@ process_tags_multi () {
 	    if (! /^</) {
 	       	# Token
 	        '"$perl_decode_entities_token"'
+		$cpos++;
 	    }
             print;
         }
@@ -302,7 +320,7 @@ extract_vrt () {
     # otherwise it would encode the & in the &lt; and &gt; produced by
     # the latter.
     $cwb_bindir/cwb-decode -Cx $corp $attr_opts |
-    $process_tags |
+    $process_tags $corp |
     vrt_decode_special_chars --xml-entities |
     eval "$head_filter" |
     $tail_filter |

commit 9663722e8b5c6cf228d3f9fec0172c0eaf4c4556

cwbdata2vrt-simple.sh: Fix when no structs with attrs
scripts/cwbdata2vrt-simple.sh:
- Fixed to work when no structures with attributes are specified and
  process_tags is "cat". (Previously, this made the script not to output
  any content but the error message "cat: corpus: No such file or
  directory", as cat was given the corpus id as an argument, which it
  should ignore. This bug was introduced in commit 1ded0fa.)

scripts/cwbdata2vrt-simple.sh
@@ -88,7 +88,7 @@ else
 	    process_tags=process_tags_single
 	fi
     else
-	process_tags=cat
+	process_tags=cat_noargs
     fi
 fi
 