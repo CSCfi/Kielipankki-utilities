55e335f56ad51a743c2e2e645d508a9634eb7c8d
eaxelson <erik.axelson@helsinki.fi>
2020-04-14 13:14:55 +0300

    Use first command line argument as directory. Do not print positional arguments or check for empty lines as they are handled in the conv.py script.

--- corp/klk-alto/alto-xml2vrt-dir.sh
+++ corp/klk-alto/alto-xml2vrt-dir.sh
@@ -3,7 +3,7 @@
 convpy="/scratch/clarin/USERNAME/Kielipankki-utilities/corp/klk-alto/conv.py";
 vrtvalidate="/scratch/clarin/USERNAME/Kielipankki-utilities/vrt-tools/vrt-validate";
 
-for xmlfile in *.xml;
+for xmlfile in $1/*.xml;
 do
     if !(echo $xmlfile | grep 'mets\.xml' > /dev/null 2> /dev/null); then
 	metsfile=`echo $xmlfile | perl -pe 's/page\-[0-9]+/mets/'`;
@@ -15,11 +15,7 @@ do
 	fi
 	vrtfile=`echo $xmlfile | perl -pe 's/\.xml/\.vrt/'`;
 	echo "Generating "$vrtfile;
-	# Remove lines with no word, i.e. an empty first field.
-	# Such lines occur after numerals, e.g. "2 000 ihmistä".
-	# Use only first field, i.e. 'word'.
-	echo "<!-- #vrt positional-attributes: word -->" > $vrtfile;
-	$convpy --mets $metsfile $xmlfile 2> /dev/null | perl -pe 'if (/^\t/) { $_="" }' | cut -f1 >> $vrtfile;
+	$convpy --mets $metsfile $xmlfile > $vrtfile
 	lines=`cat $vrtfile | wc -l`;
 	if [ "$lines" = "1" ]; then
 	    echo "Empty VRT file, renaming *.vrt -> *.vrt.empty";
