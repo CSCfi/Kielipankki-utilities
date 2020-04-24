c435061ae9d18d5d573cec44ceb71e7e2344617a
eaxelson <erik.axelson@helsinki.fi>
2020-04-24 15:24:32 +0300

    Copy VRT files to a separate directory and sort them by language. Treat all warnings from vrt-validate as errors.

--- corp/klk-alto/alto-xml2vrt-dirs-new.sh
+++ corp/klk-alto/alto-xml2vrt-dirs-new.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-convpy="Kielipankki-utilities/corp/klk-alto/conv-new.py";
-mets2metadata="Kielipankki-utilities/corp/klk-alto/mets2metadata.py";
+# Location of Kielipankki-utilities directory.
+scriptdir="."
+# Where VRT files are copied. If empty, no copying is done.
+outputdir=""
+# The linking file in csv format.
 linkingfile="FILENAME.csv"
-vrtvalidate="Kielipankki-utilities/vrt-tools/vrt-validate";
+
+convpy=$scriptdir"/Kielipankki-utilities/corp/klk-alto/conv-new.py";
+mets2metadata=$scriptdir"/Kielipankki-utilities/corp/klk-alto/mets2metadata.py";
+vrtvalidate=$scriptdir"/Kielipankki-utilities/vrt-tools/vrt-validate";
 
 for dir in $1;
 do
@@ -13,6 +19,15 @@ do
 	metadatafile=`echo $metsfile | perl -pe 's/_mets\.xml/\.metadata/;'`;
 	echo "Generating "$metadatafile;
 	$mets2metadata $metsfile $linkingfile > $metadatafile;
+	# extract language used
+	lang="";
+	if [ "$outputdir" != "" ]; then
+	    lang=`grep 'language' $metadatafile | perl -pe "s/^.*'language'\: '([^']+)'.*$/\1/;"`;
+	    echo "Language: "$lang;
+	    if ! [ -d "$outputdir/$lang" ]; then
+		mkdir $outputdir/$lang;
+	    fi
+	fi
 	# process all xml files that use the current mets file
 	xmlfiles=`echo $metsfile | perl -pe 's/_mets\.xml/_page-*.xml/;'`
 	for xmlfile in $xmlfiles;
@@ -24,12 +39,19 @@ do
 	    if [ "$lines" = "1" ]; then
 		echo "Empty VRT file, renaming *.vrt -> *.vrt.empty";
 		mv $vrtfile $vrtfile.empty;
+		vrtfile=$vrtfile.empty
 	    else
 		echo "Validating result";
-		if !($vrtvalidate $vrtfile); then
+		$vrtvalidate $vrtfile -o tmp;
+		lines=`cat tmp | wc -l`;
+		if [ "$lines" -gt "1" ]; then
+		    cat tmp;
 		    echo "Not valid VRT format, exiting.";
 		    exit 1;
 		fi
+	    fi
+	    if [ "$outputdir" != "" ]; then
+		cp --parents $vrtfile $outputdir/$lang/;
 	    fi
 	done
     done
