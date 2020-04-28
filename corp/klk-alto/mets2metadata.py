f350e3c977e8e7a0ace02150a502cffb3f50e64c
eaxelson <erik.axelson@helsinki.fi>
2020-04-28 12:20:29 +0300

    Output only binding id, not the whole url.

--- corp/klk-alto/mets2metadata.py
+++ corp/klk-alto/mets2metadata.py
@@ -15,8 +15,8 @@ if link_info == '':
     stderr.write('Error: no link info found for mets file ' + argv[1] + "\n")
     exit(1)
 else:
-    binding_info = link_info.split(',')[8].replace('"','')
-    if not binding_info.startswith('http'):
+    binding_info = link_info.split(',')[8]
+    if not binding_info.startswith('"http'):
         stderr.write('Error: no binding url could be extracted for mets file ' + argv[1] + "\n")
         exit(1)
     if '/aikakausi/' in binding_info:
@@ -27,7 +27,10 @@ else:
         stderr.write('Error: no publication type could be extracted for mets file ' + argv[1] + "\n")
         exit(1)
 
+# extract binding id from binding url
+binding_id = subprocess.getoutput('echo %s | perl -pe "s/.*binding\///; s/#.*//;"' % (binding_info))
+
 mets.update({'date': date})
-mets.update({'binding_id': binding_info})
+mets.update({'binding_id': binding_id})
 mets.update({'publ_type': publ_type})
 print(mets)
