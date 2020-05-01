f2fb69e831e22f1152ac669af68c8da3385d086b
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2020-05-01 09:05:10 +0300

    A couple of new tests for rel-tools
    Still minimal
    
    new file:   check/head.sh
    modified:   check/regret
    new file:   check/sample.sh
    new file:   check/tail.sh

---
+++ check/sample.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# a rel-tools/check/ test script
+# - call in rel-tools
+# - scratch rel-tools/tmp/
+
+source check/libtest.sh
+
+TOPIC="${0##*/}"
+TOPIC="${TOPIC%.sh}"
+
+test001 () {
+    setup $FUNCNAME
+    ./rel-sample --help \
+	       1> "$DIR/out" \
+	       2> "$DIR/err"
+    test $? = 0 -a -s "$DIR/out" -a ! -s "$DIR/err"
+    report "--help"
+    cleanup
+}
+
+test001
+
+test002 () {
+    setup $FUNCNAME
+    ./rel-sample < check/tau.tsv \
+	       1> "$DIR/out" \
+	       2> "$DIR/err"
+    test $? = 0 -a -s "$DIR/out" -a ! -s "$DIR/err"
+    report "stdin/stdout, no option"
+    cleanup
+}
+
+test002
+
+test003 () {
+    setup $FUNCNAME
+    ./rel-sample --records=3 check/tau.tsv \
+	       1> "$DIR/out" \
+	       2> "$DIR/err"
+    test $? = 0 -a -s "$DIR/out" -a ! -s "$DIR/err"
+    report "file/stdout, --records=3"
+    cleanup
+}
+
+test003
+
+test004 () {
+    setup $FUNCNAME
+    ./rel-sample --records=3 --tag=org check/tau.tsv \
+	       1> "$DIR/out" \
+	       2> "$DIR/err"
+    test $? = 0 -a -s "$DIR/out" -a ! -s "$DIR/err"
+    report "file/stdout, --records=3 --tag=org"
+    cleanup
+}
+
+test004
---
+++ check/tail.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+# a rel-tools/check/ test script
+# - call in rel-tools
+# - scratch rel-tools/tmp/
+
+source check/libtest.sh
+
+TOPIC="${0##*/}"
+TOPIC="${TOPIC%.sh}"
+
+test001 () {
+    setup $FUNCNAME
+    ./rel-tail --help \
+	       1> "$DIR/out" \
+	       2> "$DIR/err"
+    test $? = 0 -a -s "$DIR/out" -a ! -s "$DIR/err"
+    report "--help"
+    cleanup
+}
+
+test001
+
+test002 () {
+    setup $FUNCNAME
+    ./rel-tail < check/tau.tsv \
+	       1> "$DIR/out" \
+	       2> "$DIR/err"
+    test $? = 0 -a -s "$DIR/out" -a ! -s "$DIR/err"
+    report "stdin/stdout, no option"
+    cleanup
+}
+
+test002
+
+test003 () {
+    setup $FUNCNAME
+    ./rel-tail --records=3 check/tau.tsv \
+	       1> "$DIR/out" \
+	       2> "$DIR/err"
+    test $? = 0 -a -s "$DIR/out" -a ! -s "$DIR/err"
+    report "file/stdout, --records=3"
+    cleanup
+}
+
+test003
