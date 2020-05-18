bb4d023de222e3885ff76e68913fb00f85b174a9
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2020-05-18 10:39:01 +0300

    Wrote tests for failing output-file options
    modified:   tests/tools/test_Dest.py

--- vrt-tools/tests/tools/test_Dest.py
+++ vrt-tools/tests/tools/test_Dest.py
@@ -4,7 +4,10 @@ from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
 
 import fake # sibling library module to provide fake data
 
+# ./vrt-drop maps "send" to "want" but fails on "fail"
+# (omitting redundant names but requiring initial names)
 send = b''.join(fake.nameloop(120, b'foo bar baz'.split()))
+fail = b''.join(fake.nameloop(120, b'foo bar baz'.split(), sans = True))
 want = b''.join(fake.nameloop(120, b'foo bar baz'.split(), once = True))
 
 def test_000(tmp_path):
@@ -147,3 +150,186 @@ def test_006(tmp_path):
 # TODO test that all manner of invalid filename option combinations
 # FAIL as intended (with input intact and often partial tmp file,
 # error status, error message, what not)
+
+def test_007(tmp_path):
+    '''Cannot name existing file as output file'''
+    old = tmp_path / 'roska.vrt'
+    old.write_bytes(send)
+    assert old.exists()
+    proc = Popen([ './vrt-drop', '-o', str(old) ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(input = send, timeout = 3)
+    assert proc.returncode
+    assert old.exists()
+    assert not out
+    assert err
+    assert old.read_bytes() == send
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_008(tmp_path):
+    '''Cannot name sibling without named input'''
+    proc = Popen([ './vrt-drop', '-I', 'out' ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(input = send, timeout = 3)
+    assert proc.returncode
+    assert not out
+    assert err
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_009(tmp_path):
+    '''Cannot name sibling without named input'''
+    proc = Popen([ './vrt-drop', '-I', 'vrt/out' ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(input = send, timeout = 3)
+    assert proc.returncode
+    assert not out
+    assert err
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_010(tmp_path):
+    '''Cannot name existing sibling as output file'''
+    inf = tmp_path / 'roska.vrt'
+    old = tmp_path / 'roska.vrt.out'
+    inf.write_bytes(send)
+    old.write_bytes(send)
+    assert inf.exists()
+    assert old.exists()
+    proc = Popen([ './vrt-drop', '-I', 'out', str(inf) ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(timeout = 3)
+    assert proc.returncode
+    assert inf.exists()
+    assert old.exists()
+    assert not out
+    assert err
+    assert inf.read_bytes() == send
+    assert old.read_bytes() == send
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_011(tmp_path):
+    '''Cannot name existing sibling as output file'''
+    inf = tmp_path / 'roska.vrt'
+    old = tmp_path / 'roska.out'
+    inf.write_bytes(send)
+    old.write_bytes(send)
+    assert inf.exists()
+    assert old.exists()
+    proc = Popen([ './vrt-drop', '-I', 'vrt/out', str(inf) ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(timeout = 3)
+    assert proc.returncode
+    assert inf.exists()
+    assert old.exists()
+    assert not out
+    assert err
+    assert inf.read_bytes() == send
+    assert old.read_bytes() == send
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_012(tmp_path):
+    '''Leave named output in temp file upon failure'''
+    ouf = tmp_path / 'roska.vrt'
+    proc = Popen([ './vrt-drop', '-o', str(ouf) ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(input = fail, timeout = 3)
+    assert proc.returncode
+    assert not ouf.exists()
+    assert tmp_path.glob('*.tmp')
+    assert not out
+    assert err
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_013(tmp_path):
+    '''Leave sibling output in temp file upon failure'''
+    inf = tmp_path / 'roska.vrt'
+    ouf = tmp_path / 'roska.vrt.out'
+    inf.write_bytes(fail)
+    proc = Popen([ './vrt-drop', '-I', 'out', str(inf) ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(timeout = 3)
+    assert proc.returncode
+    assert inf.exists()
+    assert not ouf.exists()
+    assert tmp_path.glob('roska.vrt.out.*.tmp')
+    assert not out
+    assert err
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_014(tmp_path):
+    '''Leave sibling output in temp file upon failure'''
+    inf = tmp_path / 'roska.vrt'
+    ouf = tmp_path / 'roska.out'
+    inf.write_bytes(fail)
+    proc = Popen([ './vrt-drop', '-I', 'vrt/out', str(inf) ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(timeout = 3)
+    assert proc.returncode
+    assert inf.exists()
+    assert not ouf.exists()
+    assert tmp_path.glob('roska.out.*.tmp')
+    assert not out
+    assert err
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_015(tmp_path):
+    '''Leave in-place output in temp file upon failure'''
+    inf = tmp_path / 'roska.vrt'
+    inf.write_bytes(fail)
+    proc = Popen([ './vrt-drop', '-i', str(inf) ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(timeout = 3)
+    assert proc.returncode
+    assert inf.exists()
+    assert inf.read_bytes() == fail
+    assert tmp_path.glob('roska.vrt.*.tmp')
+    assert not out
+    assert err
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
+def test_016(tmp_path):
+    '''Leave in-place output in temp file upon failure'''
+    inf = tmp_path / 'roska.vrt'
+    bak = tmp_path / 'roska.vrt-'
+    inf.write_bytes(fail)
+    proc = Popen([ './vrt-drop', '-b-', str(inf) ],
+                 stdin = PIPE,
+                 stdout = PIPE,
+                 stderr = PIPE)
+    out, err = proc.communicate(timeout = 3)
+    assert proc.returncode
+    assert inf.exists()
+    assert not bak.exists()
+    assert inf.read_bytes() == fail
+    assert tmp_path.glob('roska.vrt.*.tmp')
+    assert not out
+    assert err
+    # also there should not be any other files there
+    # (nor any stray effects anywhere else)
+
