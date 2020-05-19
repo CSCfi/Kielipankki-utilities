8a239440486f3f4f3e8e4b7272c78298a9c8c42f
eaxelson <erik.axelson@helsinki.fi>
2020-05-19 12:57:55 +0300

    Add option --dry-run to get-txtfilenames.sh. Treat warnings as errors.

--- corp/ethesis/retokenize_en/get-txtfilenames.sh
+++ corp/ethesis/retokenize_en/get-txtfilenames.sh
@@ -9,11 +9,13 @@
 
 if [ "$1" = "--help" -o "$1" = "-h" ]; then
     echo """
-get-txtfilenames.sh CORPUSDIR TSVFILE [VRTFILE]
+get-txtfilenames.sh CORPUSDIR TSVFILE VRTFILE TARGETDIR [--dry-run]
 
 For each pdfurl and date given in TSVFILE, find the corresponding
-text file in directory CORPUSDIR. If VRTFILE is given, print the
-number of lines for the text in that file.
+text file (TXTFILE.txt) in directory CORPUSDIR and the line in
+VRTFILE that contains the metadata for that text, i.e. the line
+beginning with \"<text \". Copy TXTFILE.txt and the metadata line
+(written to file TXTFILE.metadata) under TARGETDIR/CORPUSDIR/.
 
 CORPUSDIR: The directory that contains the text files.
 TSVFILE:   A file that contains the pdfurls and dates of the texts,
@@ -34,56 +36,85 @@ done
 corpusdir=$1;
 tsvfile=$2;
 vrtfile=$3;
+targetdir=$4;
+dry_run="false";
+if [ "$5" = "--dry-run" ]; then
+    dry_run="true";
+fi
+
+if [ -d "$targetdir" -a "$dry_run" = "false" ]; then
+    echo "Error: TARGETDIR $targetdir exists.";
+    exit 1;
+fi
+if [ "$dry_run" = "false" ]; then
+    mkdir $targetdir;
+    mkdir $targetdir/$corpusdir;
+fi
 
 while read line
 do
-    echo "---";
     found=0;
     # English marked with "en"
     txtfilename=`echo "$line" | ./pdfurl-date-to-txtfilename.pl "en"`;
     if [ "$txtfilename" != "" ]; then
-	if (ls $corpusdir/$txtfilename 2> /dev/null); then found=$((found + 1)); fi
+	if [ -f "$corpusdir/$txtfilename" ]; then found=$((found + 1)); fi
     fi
     # English marked with "eng"
     txtfilename=`echo "$line" | ./pdfurl-date-to-txtfilename.pl "eng"`;
     if [ "$txtfilename" != "" ]; then
-	if (ls $corpusdir/$txtfilename 2> /dev/null); then found=$((found + 1)); fi
+	if [ -f "$corpusdir/$txtfilename" ]; then found=$((found + 1)); fi
     fi
     # Language not marked
     txtfilename=`echo "$line" | ./pdfurl-date-to-txtfilename.pl ""`;
     if [ "$txtfilename" != "" ]; then
-	if (ls $corpusdir/$txtfilename 2> /dev/null); then found=$((found + 1)); fi
+	if [ -f "$corpusdir/$txtfilename" ]; then found=$((found + 1)); fi
     fi
     # No date or language marked
     txtfilename=`echo "$line" | ./pdfurl-to-txtfilename.pl`;
     if [ "$txtfilename" != "" ]; then
-	if (ls $corpusdir/$txtfilename 2> /dev/null); then found=$((found + 1)); fi
+	if [ -f "$corpusdir/$txtfilename" ]; then found=$((found + 1)); fi
     fi
 
-    if [ "$found" -gt "1" ]; then echo "Warning: ^^^ several txtfiles found for $line ^^^"; fi
+    if [ "$found" -gt "1" ]; then
+	echo "Error: several txtfiles found for $line.";
+	if [ "$dry_run" = "false" ]; then exit 1; fi;
+    fi
     if [ "$found" -eq "0" ]; then
 	# Try any language
 	txtfilename=`echo "$line" | ./pdfurl-date-to-txtfilename.pl "*"`;
 	if [ "$txtfilename" != "" ]; then
-	    if (ls $corpusdir/$txtfilename 2> /dev/null); then
-		echo "Warning: ^^^ txtfile(s) found, but language is not English ^^^";
-		found=$((found + 1));
+	    if [ -f "$corpusdir/$txtfilename" ]; then
+		echo "Error: txtfile(s) found, but language is not English:";
+		ls $corpusdir/$txtfilename;
+		if [ "$dry_run" = "false" ]; then exit 1; fi;
 	    else
-		echo "Error: txtfile not found for $line";
+		echo "Error: txtfile not found for $line.";
+		if [ "$dry_run" = "false" ]; then exit 1; fi;
 	    fi
 	else
-	    echo "Error: txtfile not found for $line";
+	    echo "Error: txtfile not found for $line.";
+	    if [ "$dry_run" = "false" ]; then exit 1; fi;
 	fi
     fi
 
-    # How many lines there are in the VRT file
-    if [ "$vrtfile" != "" -a  "$found" -gt "0" ]; then
-	expr=`echo $line | cut -f1 -d' '`;
-	start=`grep --line-number $expr $vrtfile | cut -f1 -d':'`;
-	metadataline=`grep $expr $vrtfile`;
-	lines=`tail -n +$start $vrtfile | egrep -m 1 --line-number '^</text>' | cut -f1 -d':'`;
-	# echo $lines;
-	echo $metadataline;
+    expr=`echo $line | cut -f1 -d' '`;
+    metadatafile=`echo $txtfilename | perl -pe 's/\.txt/.metadata/;'`;
+    metadatafile=$targetdir/$corpusdir/$metadatafile;
+    if [ "$dry_run" = "true" ]; then
+	metadatafile=/dev/null;
     fi
+    if ! (grep --fixed-strings $expr $vrtfile > $metadatafile); then
+	echo "Error: no metadata found for file $corpusdir/$txtfilename.";
+	if [ "$dry_run" = "false" ]; then exit 1; fi;
+    fi
+    if [ "$dry_run" = "false" ]; then
+	if ! (cp $corpusdir/$txtfilename $targetdir/$corpusdir/$txtfilename); then
+	    echo "Error: could not copy file $corpusdir/$txtfilename.";
+	    exit !;
+	fi
+    fi
+    # start=`grep --line-number $expr $vrtfile | cut -f1 -d':'`;
+    # lines=`tail -n +$start $vrtfile | egrep -m 1 --line-number '^</text>' | cut -f1 -d':'`;
+    # echo $lines;
 
 done < ${tsvfile};
