a1aaa6050cf971bfc8f7ae79e7dad710660985a8
eaxelson <erik.axelson@helsinki.fi>
2020-06-08 20:40:26 +0300

    Remove or replace strange characters and charater combinations..

--- corp/ethesis/retokenize_en/new_scripts/fix-special-characters.pl
+++ corp/ethesis/retokenize_en/new_scripts/fix-special-characters.pl
@@ -9,8 +9,12 @@ foreach my $line ( <STDIN> ) {
     #   Unicode line and paragraph separators (U+2028, U+2029) and soft hyphens (U+00AD) and some other strange characters
     # - convert FIGURE SPACE (U+2007) and NARROW NO-BREAK SPACE (U+202F) (and also THIN SPACE, U+2009) and NBSP (U+00A0)
     #   as well as other spaces into ordinary spaces
-    $line =~ s/[\x{0000}-\x{0008}\x{000B}\x{000C}\x{000E}-\x{001F}\x{00AD}\x{007F}-\x{009F}\x{2028}\x{2029}\x{00AD}\x{2028}\x{FEFF}\x{FFFF}]//g;
+    $line =~ s/[\x{0000}-\x{0008}\x{000B}\x{000C}\x{000E}-\x{001F}\x{00AD}\x{007F}-\x{009F}\x{2028}\x{2029}\x{00AD}\x{2028}\x{FEFF}\x{FFFF}\x{FFFD}]//g;
     $line =~ s/[\x{2007}\x{202F}\x{2009}\x{00A0}]/ /g;
     $line =~ s/[\x{0085}\x{1680}\x{2000}-\x{200A}\x{202F}\x{205F}\x{3000}]/ /g;
+    # Replace HTML entities between 0-31 with space. They sometimes separate words.
+    $line =~ s/&#[0-9];/ /g;
+    $line =~ s/&#[12][0-9];/ /g;
+    $line =~ s/&#3[01];/ /g;
     print $line;	
 }
