f8d09d5dbdacf5fc1cc6f9e81243af44cd2ccf42
eaxelson <erik.axelson@helsinki.fi>
2020-10-14 16:21:26 +0300

    Add optional parameter for directory. Use option -e in echo.

--- corp/klk-alto/batch_scripts/list-dirs.sh
+++ corp/klk-alto/batch_scripts/list-dirs.sh
@@ -3,14 +3,25 @@
 
 if [ "$1" = "--help" -o "$1" = "-h" ]; then
     echo """
-For each directory in */*/alto, output its size in kilobytes
+list-dirs.sh [DIR]
+
+For each directory in DIR/*/*/alto, output its size in kilobytes
 and name, separated by a tab, in ascending order.
 """
     exit 0;
 fi
 
-for dir in */*/alto*;
+DIR=".";
+if [ "$1" != "" ]; then
+    if ! [ -d "$1" ]; then
+	echo "No such directory: $1, exiting.";
+    else
+	DIR=$1;
+    fi
+fi
+
+for dir in $DIR/*/*/alto*;
 do
     kbytes=`du -c -k -d 0 $dir/*.xml | tail -1 | cut -f1`;
-    echo $kbytes"\t"$dir;
+    echo -e $kbytes"\t"$dir;
 done | sort -n;
