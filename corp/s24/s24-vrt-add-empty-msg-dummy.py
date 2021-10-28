d61c72956ad779714487755ec1666bfa4e6c99fa
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-10-28 06:15:32 +0300

    s24-vrt-add-empty-msg-dummy: Modify "empty" attrs
    corp/s24/s24-vrt-add-empty-msg-dummy.py:
    - Reflect text attributes to those of the final version of the Suomi24
      VRT:
      - Change text attribute empty_message="true" to empty="y" and paragraph
        attribute type="empty_message" to type="empty".
      - Add empty="n" to non-empty texts.
    - Update usage description to mention the attributes.

--- corp/s24/s24-vrt-add-empty-msg-dummy.py
+++ corp/s24/s24-vrt-add-empty-msg-dummy.py
@@ -13,28 +13,41 @@ class DummyContentAdder(vrtargsoolib.InputProcessor):
 
     DESCRIPTION = """
     Replace completely empty messages (empty text structures) in VRT
-    with a dummy text in a sentence and paragraph.
+    with a dummy text in a sentence and paragraph (with type="empty").
+    Add attribute empty="y" to empty text structures and empty="n" to
+    non-empty ones.
     """
 
     def __init__(self):
         super().__init__()
 
     def main(self, args, inf, ouf):
-        dummy_content = """<paragraph type="empty_message">
+        dummy_content = """<paragraph type="empty">
 <sentence>
 _\t_\tPunct\t_\t1\t0\tROOT\t_\t1
 </sentence>
 </paragraph>
 """.encode()
+        empty_attr = {
+            True: b' empty="y"',
+            False: b' empty="n"',
+        }
 
         def output_lines(lines):
             for line in lines:
                 ouf.write(line)
 
-        def make_dummy(text):
+        def add_empty_attr(line, empty):
             # Assume \n line terminator and no trailing whitespace
-            text[0] = text[0][:-2] + b' empty_message="true"' + text[0][-2:]
+            return line[:-2] + empty_attr[empty] + line[-2:]
+
+        def make_dummy(text):
+            text[0] = add_empty_attr(text[0], True)
             text[1:1] = [dummy_content]
+            return text
+
+        def make_nonempty(text):
+            text[0] = add_empty_attr(text[0], False)
             return text
 
         text = []
@@ -42,9 +55,10 @@ _\t_\tPunct\t_\t1\t0\tROOT\t_\t1
             text.append(line)
             if line.startswith(b'</text>'):
                 if (len(text) == 2):
-                    output_lines(make_dummy(text))
+                    text = make_dummy(text)
                 else:
-                    output_lines(text)
+                    text = make_nonempty(text)
+                output_lines(text)
                 text = []
 
 
