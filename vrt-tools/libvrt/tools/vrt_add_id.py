25ddd2841e42100f7e7c33c041d3cc79cdf5415c
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2024-01-17 10:51:26 +0200

    libvrt.tools.vrt_add_id: Support unique random ids
    vrt-tools/libvrt/tools/vrt_add_id.py:
    - Add --random: Ids are unique random 32-bit integers represented as 8
      hex digits.
    - Add --seed: Specify random number generator seed; default to
      non-reproducible.
    - Add --counter (default): Ids are integers based on a counter.

--- vrt-tools/libvrt/tools/vrt_add_id.py
+++ vrt-tools/libvrt/tools/vrt_add_id.py
@@ -4,6 +4,7 @@
 
 from argparse import ArgumentTypeError
 from itertools import count
+import random
 import re
 
 from libvrt.args import BadData
@@ -11,6 +12,8 @@ from libvrt.args import transput_args
 
 from libvrt.metaname import nametype # need checked
 from libvrt.metaline import mapping, starttag
+
+DEFAULT_RAND_END = pow(2, 32)
 
 def affix(arg):
     if re.fullmatch('[A-Za-z0-9_\-+/.:]*', arg):
@@ -22,7 +25,7 @@ def parsearguments(argv, *, prog = None):
     description = '''
 
     Add or overwrite an "id" attribute to each element of the
-    specified kind, based on a counter.
+    specified kind, based on a counter or unique random values.
 
     '''
 
@@ -43,6 +46,39 @@ def parsearguments(argv, *, prog = None):
                         help = '''
 
                         name of the "id" attribute ("id")
+
+                        ''')
+
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument('--counter',
+                       action = 'store_const',
+                       dest = 'type',
+                       const = 'counter',
+                       help = '''
+
+                       id values are integers based on a counter (the
+                       default)
+
+                        ''')
+
+    group.add_argument('--random',
+                       action = 'store_const',
+                       dest = 'type',
+                       const = 'random',
+                       help = '''
+
+                       id values are unique 32-bit random integers (8
+                       hex digits)
+
+                       ''')
+
+    parser.add_argument('--seed', metavar = 'string',
+                        default = '',
+                        help = '''
+
+                        random number generator seed for random ids
+                        (default: "" = non-reproducible)
 
                         ''')
 
@@ -80,6 +116,10 @@ def parsearguments(argv, *, prog = None):
                         ''')
 
     args = parser.parse_args()
+    if not args.type:
+        args.type = 'counter'
+    if not args.seed:
+        args.seed = None
     args.prog = prog or parser.prog
 
     return args
@@ -87,21 +127,59 @@ def parsearguments(argv, *, prog = None):
 def main(args, ins, ous):
     '''Transput VRT (bytes) in ins to VRT (bytes) in ous.'''
 
-    ids = (
-        '{}{}'.format(args.prefix, k).encode('UTF-8')
-        for k in count(start = args.start)
-    )
+    ids = get_idgen(args)
 
     kind = (b''.join((b'<', args.element, b'>')),
             b''.join((b'<', args.element, b' ')))
+
+    fmt = '{}{:08x}' if args.type == 'random' else '{}{}'
 
     for line in ins:
         if line.startswith(kind):
             attrs = mapping(line)
             if args.force or args.idn not in attrs:
-                attrs[args.idn] = next(ids)
+                attrs[args.idn] = (
+                    fmt.format(args.prefix, next(ids)).encode('UTF-8'))
             else:
                 raise BadData('element has id already')
             ous.write(starttag(args.element, attrs, sort=args.sort))
         else:
             ous.write(line)
+
+def get_idgen(args):
+    '''Return id generator based on args.'''
+
+    if args.type == 'counter':
+        # Generator for an integer counter
+        return (
+            k for k in count(start = args.start)
+        )
+
+    else:
+        # Multiple generators use different seeds
+        if args.seed:
+            args.seed += '1'
+        return randint_uniq(DEFAULT_RAND_END, args.seed)
+
+def randint_uniq(end, seed=None):
+    '''Generator for unique random integers in [0, end[ with seed.'''
+
+    # Values already generated
+    used = set()
+    rnd = random.Random(seed)
+    errmsg = f'more than {end} elements encountered'
+
+    while True:
+        if len(used) >= end:
+            raise BadData(errmsg)
+        r = rnd.randrange(0, end)
+        i = 0
+        while r in used:
+            r = rnd.randrange(0, end)
+            i += 1
+            # If 1000 consecutive random numbers have already been
+            # used, raise an error (1000 is arbitrary)
+            if i > 1000:
+                raise BadData(errmsg)
+        used.add(r)
+        yield r
