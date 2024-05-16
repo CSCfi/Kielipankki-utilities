002a2aa95cee2acd2a02c81e39a5399b5441fada
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2024-05-16 14:50:41 +0300

    vrt-drop-empty: Add --verbose

--- vrt-tools/libvrt/tools/vrt_drop_empty.py
+++ vrt-tools/libvrt/tools/vrt_drop_empty.py
@@ -9,7 +9,9 @@ Please run "vrt-drop-empty -h" for more information.
 
 
 import re
+import sys
 
+from collections import defaultdict
 from itertools import groupby
 
 from vrtargsoolib import InputProcessor
@@ -25,12 +27,17 @@ class VrtEmptyStructDropper(InputProcessor):
     represented in CWB data and are ignored by cwb-encode. XML-style
     comment lines within empty structures are retained.
     """
+    ARGSPECS = [
+        ('--verbose|-v',
+         '''output the number of dropped structures to stderr''')
+    ]
 
     def __init__(self):
         super().__init__()
 
     def main(self, args, inf, ouf):
         """Read inf, write to ouf, with command-line arguments args."""
+        drop_counts = defaultdict(int)
 
         def drop_empty(lines):
             """Return generator iterating over lines not forming empty struct."""
@@ -44,6 +51,7 @@ class VrtEmptyStructDropper(InputProcessor):
                         drop_line_nums.add(linenum)
                         drop_line_nums.add(struct_stack[-1][1])
                         struct_stack.pop()
+                        drop_counts[struct] += 1
                 elif not line.startswith(b'<!'):
                     # Start tag
                     struct = line[1:].partition(b' ')[0].rstrip(b'>\n')
@@ -51,9 +59,21 @@ class VrtEmptyStructDropper(InputProcessor):
             return (line for linenum, line in enumerate(lines)
                     if linenum not in drop_line_nums)
 
+        def output_drop_counts():
+            """Output the number of structures dropped."""
+            if not drop_counts:
+                sys.stderr.write('No structures dropped\n')
+            else:
+                sys.stderr.write('Dropped structures:\n')
+                for struct in sorted(drop_counts.keys()):
+                    sys.stderr.write(
+                        f'  {struct.decode("UTF-8")}: {drop_counts[struct]}\n')
+
         LT = b'<'[0]
         for istag, lines in groupby(inf, lambda line: line[0] == LT):
             if istag:
                 lines = drop_empty(list(lines))
             for line in lines:
                 ouf.write(line)
+        if args.verbose:
+            output_drop_counts()
