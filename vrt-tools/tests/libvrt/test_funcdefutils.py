b05dab077fc952767f77988ba40578d4fce34e18
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2025-02-05 11:34:11 +0200

    funcdefutils tests: Perl-style subst testcases as module vars
    vrt-tools/tests/libvrt/test_funcdefutils.py:
    - Define module-level global variables for the Pytest parameters of
      Perl-style substitution testcases, so that they can be reused.

--- vrt-tools/tests/libvrt/test_funcdefutils.py
+++ vrt-tools/tests/libvrt/test_funcdefutils.py
@@ -11,6 +11,38 @@ import pytest
 import libvrt.funcdefutils as fdu
 
 
+# Input string for testing Perl-style substitution expressions
+_perl_subst_input = 'aaa abc bbc AAb'
+
+# Parametrized test cases for Perl-style substitution expressions:
+# expression, result (for _perl_subst_input), regexp, repl, count,
+# flags
+_perl_subst_testcases = [
+    # Base case
+    ('s/a+/z/', 'z abc bbc AAb', 'a+', 'z', '1', '0'),
+    # /g
+    ('s/[ab]+/z/g', 'z zc zc AAz', '[ab]+', 'z', '0', '0'),
+    # /i
+    ('s/[ab]+/z/gi', 'z zc zc z', '[ab]+', 'z', '0', 're.I'),
+    # /x
+    ('s/ [ab] + \t/z/x', 'z abc bbc AAb', ' [ab] + \t', 'z', '1', 're.X'),
+    # /xig (multiple flags)
+    ('s/ [ab] + \t/z/xig', 'z zc zc z', ' [ab] + \t', 'z', '0', 're.X|re.I'),
+    # Group reference
+    ('s/(a+)(b+)/$2-$1/ig', 'aaa b-ac bbc b-AA', '(a+)(b+)', r'\2-\1', '0',
+     're.I'),
+    # TODO: Test /l (run-time locale rules)
+]
+
+_perl_subst_testcases_invalid = [
+    # Only one slash
+    's/a',
+    # Only two slashes
+    's/a/s',
+    # Invalid flag
+    's/a/s/z',
+]
+
 class TestDefineTransformFunc:
 
     """Tests for function define_transform_func."""
@@ -23,30 +55,13 @@ class TestDefineTransformFunc:
 
     @pytest.mark.parametrize(
         'code,result,regexp,repl,count,flags',
-        [
-            # Base case
-            ('s/a+/z/', 'z abc bbc AAb', 'a+', 'z', '1', '0'),
-            # /g
-            ('s/[ab]+/z/g', 'z zc zc AAz', '[ab]+', 'z', '0', '0'),
-            # /i
-            ('s/[ab]+/z/gi', 'z zc zc z', '[ab]+', 'z', '0', 're.I'),
-            # /x
-            ('s/ [ab] + \t/z/x', 'z abc bbc AAb', ' [ab] + \t', 'z', '1',
-             're.X'),
-            # /xig (multiple flags)
-            ('s/ [ab] + \t/z/xig', 'z zc zc z', ' [ab] + \t', 'z', '0',
-             're.X|re.I'),
-            # Group reference
-            ('s/(a+)(b+)/$2-$1/ig', 'aaa b-ac bbc b-AA', '(a+)(b+)', r'\2-\1',
-             '0', 're.I'),
-            # TODO: Test /l (run-time locale rules)
-        ]
+        _perl_subst_testcases
     )
     def test_define_transform_func_perl_subst(self, code, result, regexp, repl,
                                               count, flags):
         """Test define_transform_func with a Perl-style regexp substitution."""
         func, funcdef = fdu.define_transform_func(code)
-        assert func('aaa abc bbc AAb') == result
+        assert func(_perl_subst_input) == result
         assert funcdef == (f'def transfunc(val):\n  return re.sub('
                            f'r"""{regexp}""", r"""{repl}""", val, {count},'
                            f' {flags})')
@@ -69,14 +84,7 @@ class TestDefineTransformFunc:
 
     @pytest.mark.parametrize(
         'code',
-        [
-            # Only one slash
-            's/a',
-            # Only two slashes
-            's/a/s',
-            # Invalid flag
-            's/a/s/z',
-        ]
+        _perl_subst_testcases_invalid
     )
     def test_define_transform_func_perl_subst_error(self, code):
         """Test define_transform_func with invalid Perl-style substitution."""
