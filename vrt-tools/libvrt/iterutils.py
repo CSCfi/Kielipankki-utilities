f010667884c5d4baf80622681a449518daf4e0c5
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2025-09-03 17:33:46 +0300

    libvrt.iterutils: Add function make_unique

--- vrt-tools/libvrt/iterutils.py
+++ vrt-tools/libvrt/iterutils.py
@@ -14,3 +14,17 @@ def find_duplicates(*iters):
     """Return list of items occurring more than once in iterables *iters."""
     counts = Counter(chain(*iters))
     return [item for item, cnt in counts.items() if cnt > 1]
+
+
+def make_unique(*iters):
+    """Return a list of items in iterables *iters, each only once.
+
+    The result contains the items in the order of their first occurrence.
+    """
+    seen = set()
+    result = []
+    for item in chain(*iters):
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
