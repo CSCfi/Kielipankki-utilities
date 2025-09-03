b81d2aaabba2bd18890a0ae027a3b36cf69a49b7
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2025-09-03 17:33:46 +0300

    libvrt.iterutils: Add function make_unique

--- vrt-tools/tests/libvrt/test_iterutils.py
+++ vrt-tools/tests/libvrt/test_iterutils.py
@@ -31,3 +31,26 @@ class TestFindDuplicates:
     def test_find_duplicates(self, input, expected):
         """Test find_duplicates() with various inputs."""
         assert iu.find_duplicates(*input) == expected
+
+
+class TestMakeUnique:
+
+    """Tests for vrtlib.iterutils function make_unique."""
+
+    @pytest.mark.parametrize(
+        'input,expected', [
+            ([], []),
+            ([[]], []),
+            ([[0, 1, 2]], [0, 1, 2]),
+            ([[0, 1, 2, 1]], [0, 1, 2]),
+            ([[0, 1, 2, 0, 1]], [0, 1, 2]),
+            ([[0, 1, 2], [0, 1]], [0, 1, 2]),
+            ([[0, 1, 2], [0, 1], [1, 2], [2, 3]], [0, 1, 2, 3]),
+            ([[0, 2, 1], [0, 1], [1, 2], [3, 2], [4]], [0, 2, 1, 3, 4]),
+            ([[0, 1, 2], {0: 3, 1: 4}.keys()], [0, 1, 2]),
+            ([['a', 'b'], ['b', 'c']], ['a', 'b', 'c']),
+        ]
+    )
+    def test_make_unique(self, input, expected):
+        """Test make_unique() with various inputs."""
+        assert iu.make_unique(*input) == expected
