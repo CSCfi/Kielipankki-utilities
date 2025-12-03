fcd9dc48f233f8d797e68b72ee2a185264c4a68f
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2025-12-03 14:10:26 +0200

    shlib/kielipankki.sh: _get_auth_info: Support SQL files
    scripts/shlib/kielipankki.sh:
    - _get_auth_info, get_licence_type, get_lbr_id:
      - Try to obtain the auth information from an SQL file if no TSV file
        is found and before trying to access the actual database.
      - Add option --sql-dir to specify an explicit directory for SQL files.
      - For the default TSV directory, first try .../vrt/$corpus_id, and if
        it does not exist of if no auth tables are found there, try
        .../sql/$corpus_id. (Previously, only the existence of the directory
        was checked, not that of the file.)
      - Use function newest_file (defined in file.sh).

--- scripts/shlib/kielipankki.sh
+++ scripts/shlib/kielipankki.sh
@@ -66,67 +66,100 @@ make_lbr_id () {
 }
 
 
-# _get_auth_info [--tsv-dir tsv_dir] corpus_id table_name value_column_name
-#                value_column_num
+# _get_auth_info [--tsv-dir tsv_dir] [--sql-dir sql_dir] corpus_id
+#                table_name value_column_name value_column_num
 #
 # Get the value of an authorization info item for corpus corpus_id
 # from the Korp auth database, either from data exported to TSV or
-# directly using the mysql client. table_name is the SQL table name
-# (and part of the TSV filename, without the "auth_" prefix) with the
-# information, value_column_name the name of the column containing the
-# information and value_column_num its number. If --tsv-dir is
-# specified, get the information from the TSV files in tsv_dir,
-# otherwise in the default TSV directory. If the TSV file does not
-# exist, try to get the information from the korp_auth MySQL database
+# dumped to SQL or directly using the mysql client. table_name is the
+# SQL table name (and part of the TSV filename, without the "auth_"
+# prefix) with the information, value_column_name the name of the
+# column containing the information and value_column_num its number.
+# If --tsv-dir is specified, check TSV files in tsv_dir, otherwise in
+# the default TSV directory; likewise for --sql-dir. If no TSV file
+# exists, try to get the information from an SQL file; if no SQL file
+# exists, try to get the information from the korp_auth MySQL database
 # if accessible.
 _get_auth_info () {
-    local tsv_dir tsv_file corpus_id table_name value_colname value_colnum \
-          corpus_u
-    if [ "$1" = "--tsv-dir" ]; then
-        # Explicit TSV dir
-        tsv_dir=$1
-        shift 2
-        corpus_id=$1
-    else
-        # Default TSV dir
-        corpus_id=$1
-        tsv_dir=$corpus_root/vrt/$corpus_id
-        if [ ! -d "$tsv_dir" ]; then
-            tsv_dir=$corpus_root/sql/$corpus_id
+    local tsv_dir sql_dir corpus_id table_name value_colname value_colnum \
+          tsv_file sql_file middir corpus_u
+    while [ "$1" != "${1#--}" ]; do
+        if [ "$1" = "--tsv-dir" ]; then
+            # Explicit TSV dir
+            tsv_dir=$1
+            shift 2
+        elif [ "$1" = "--sql-dir" ]; then
+            # Explicit SQL dir
+            sql_dir=$1
+            shift 2
+        else
+            lib_error "Unknown option to _get_auth_info: $1"
         fi
-    fi
+    done
+    corpus_id=$1
     table_name=$2
     value_colname=$3
     value_colnum=$4
     corpus_u=$(toupper $corpus_id)
-    tsv_file=$(
-        ls -t "$tsv_dir/${corpus_id}_auth_$table_name".tsv* 2> /dev/null |
-            head -1)
+    if [ "x$tsv_dir" != x ]; then
+        tsv_file=$(newest_file "$tsv_dir/${corpus_id}_auth_$table_name".tsv*)
+    else
+        # Default TSV dir: first try .../vrt/..., then .../sql/... if
+        # needed
+        for middir in vrt sql; do
+            tsv_dir=$corpus_root/$middir/$corpus_id
+            if [ -d "$tsv_dir" ]; then
+                tsv_file=$(
+                    newest_file "$tsv_dir/${corpus_id}_auth_$table_name".tsv*)
+            fi
+            if [ "x$tsv_file" != x ]; then
+                break
+            fi
+        done
+    fi
     if [ "x$tsv_file" != x ]; then
         comprcat "$tsv_file" | head -1 | cut -f$value_colnum
-    elif [ "x$mysql_bin" != x ]; then
-        run_mysql --auth "SELECT $value_colname FROM auth_$table_name WHERE corpus='$corpus_u';" --skip-column-names
+    else
+        if [ "x$sql_dir" = x ]; then
+            # Default SQL dir
+            sql_dir=$corpus_root/sql/$corpus_id
+        fi
+        sql_file=$(newest_file "$sql_dir/${corpus_id}_auth".sql*)
+        if [ "x$sql_file" != x ]; then
+            # Find the values line after the INSERT, split it into
+            # columns and choose the right one; this assumes that the
+            # values do not contain any of the characters ' , ( ) ;
+            comprcat "$sql_file" |
+                grep -A1 "INSERT INTO \`auth_$table_name\` VALUES" |
+                tail -1 |
+                sed -e "s/','/\t/g; s/['();]//g" |
+                cut -f$value_colnum
+        elif [ "x$mysql_bin" != x ]; then
+            run_mysql --auth "SELECT $value_colname FROM auth_$table_name WHERE corpus='$corpus_u';" --skip-column-names
+        fi
     fi
 }
 
-# get_licence_type [--tsv-dir tsv_dir] corpus_id
+# get_licence_type [--tsv-dir tsv_dir] [--sql-dir sql_dir] corpus_id
 #
 # Get the licence type of corpus corpus_id from the Korp auth
-# database. If --tsv-dir is specified, get the information from the
-# TSV files in tsv_dir, otherwise in the default TSV directory. If the
-# TSV file does not exist, try to get the information from the
-# korp_auth MySQL database if accessible.
+# database. If --tsv-dir is specified, check TSV files in tsv_dir,
+# otherwise in the default TSV directory; likewise for --sql-dir. If
+# no TSV file exists, try to get the information from an SQL file; if
+# no SQL file exists, try to get the information from the korp_auth
+# MySQL database if accessible.
 get_licence_type () {
     _get_auth_info "$@" license license 2
 }
 
-# get_lbr_id [--tsv-dir tsv_dir] corpus_id
+# get_lbr_id [--tsv-dir tsv_dir] [--sql-dir sql_dir] corpus_id
 #
 # Get the LBR id of corpus corpus_id from the Korp auth database. If
-# --tsv-dir is specified, get the information from the TSV files in
-# tsv_dir, otherwise in the default TSV directory. If the TSV file
-# does not exist, try to get the information from the korp_auth MySQL
-# database if accessible.
+# --tsv-dir is specified, check TSV files in tsv_dir, otherwise in the
+# default TSV directory; likewise for --sql-dir. If no TSV file
+# exists, try to get the information from an SQL file; if no SQL file
+# exists, try to get the information from the korp_auth MySQL database
+# if accessible.
 get_lbr_id () {
     _get_auth_info "$@" lbr_map lbr_id 1
 }
