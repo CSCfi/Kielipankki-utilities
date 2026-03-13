8f14d82189b8e395130d7d4f937162d7de87f79a
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2026-03-13 17:27:47 +0200

    vrt-meta: Add --no-tag: Do not add tag field nor uniquify

--- vrt-tools/libvrt/tools/vrtmeta.py
+++ vrt-tools/libvrt/tools/vrtmeta.py
@@ -23,7 +23,8 @@ def parsearguments(argv, *, prog = None):
     Present the attribute values of a specified element (default text)
     in a VRT document as a relation in the form of a Tab-Separated
     Values (TSV) document, complete with a head and unique rows in the
-    body. Initially identified attribute names become field names, the
+    body. (Uniqueness is not guaranteed when using --no-tag.)
+    Initially identified attribute names become field names, the
     corresponding values become the content of records. This is not a
     VRT validator.
 
@@ -92,6 +93,15 @@ def parsearguments(argv, *, prog = None):
 
                        ''')
 
+    group.add_argument('--no-tag', action = 'store_true',
+                       help = '''
+
+                       do not add a tag field nor omit duplicate
+                       records (if the output contains duplicates, it
+                       is not a relation)
+
+                       ''')
+
     args = parser.parse_args()
     args.prog = prog or parser.prog
 
@@ -137,6 +147,19 @@ def main(args, ins, ous):
         ship(values(line), 1)
         for k, line in enumerate(data, start = 2):
             ship(values(line), k)
+        else:
+            return 0
+    elif args.no_tag:
+
+        def ship(rec):
+            ous.write(b'\t'.join(rec))
+            ous.write(b'\n')
+            return
+
+        ship(head)
+        ship(values(line))
+        for line in data:
+            ship(values(line))
         else:
             return 0
     else:
